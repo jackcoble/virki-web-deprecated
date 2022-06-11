2c9f2cc0858bfa863cd4c940c35ebcd8ee80078b
Jack <jack@virki.io>
2022-06-11 15:14:41 +0100

    Update tab icons and support for custom classes

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,9 +1,8 @@
 <template>
-<nav class="fixed lg:hidden bottom-0 p-1.5 w-full border bg-gray-100 flex">
-    <div v-for="button in buttons" :key="button.label" class="flex flex-col flex-grow w-full items-center text-center truncate">
+<nav class="fixed lg:hidden bottom-0 p-2 w-full border bg-gray-100 flex">
+    <div v-for="button in buttons" :key="button.label" class="flex flex-col flex-grow w-full justify-center items-center text-center truncate">
         <router-link :to="button.route">
-            <component class="h-6 text-gray-600 mx-auto" :is="button.icon" />
-            <span class="text-xs align-middle font-semibold">{{ button.label }}</span>
+            <component class="h-6 text-gray-600 mx-auto" :class="button.classes" :is="button.icon" />
         </router-link>
     </div>
 </nav>
@@ -11,26 +10,24 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-
-import { ClockIcon, QrcodeIcon, CogIcon } from "@heroicons/vue/solid";
+import { HomeIcon, PlusCircleIcon, CogIcon } from "@heroicons/vue/solid";
 
 const buttons = ref([
     {
         label: "Tokens",
-        route: "/vault",
-        icon: ClockIcon
+        route: "/",
+        icon: HomeIcon
     },
     {
         label: "New",
         route: "/new",
-        icon: QrcodeIcon
+        icon: PlusCircleIcon,
+        classes: "h-12 text-green-600"
     },
     {
         label: "Settings",
         route: "/settings",
         icon: CogIcon
     }
-
-
 ])
 </script>
