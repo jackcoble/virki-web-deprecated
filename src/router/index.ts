f6c01558dffb4210037a46678e683fd1692a16e7
Jack <jack@virki.io>
2022-06-11 17:59:10 +0100

    Structure of "new" page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
 import Login from "@/views/Login.vue"
+import New from "@/views/new/Index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: "/new",
+      name: "new",
+      component: New
     }
   ]
 })
---
+++ src/views/new/Index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="h-screen p-4 bg-[#262727] space-y-3">
+        <h2 class="text-2xl font-semibold text-white">Create</h2>
+
+        <!-- Scan QR Code -->
+        <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
+            <QrcodeIcon class="h-12 mr-4"></QrcodeIcon>
+            <div class="flex-row">
+                <h2 class="text-xl font-bold">Scan</h2>
+                <p class="text-sm">If you have a QR code of your 2FA, you can quickly scan it in here.</p>
+            </div>
+        </div>
+
+        <!-- Create token vault -->
+        <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
+            <LockClosedIcon class="h-12 mr-4" />
+            <div class="flex-row">
+                <h2 class="text-xl font-bold">Vault</h2>
+                <p class="text-sm">Create a new token vault to store your authentication codes.</p>
+            </div>
+        </div>
+
+        <!-- Advanced entry -->
+        <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
+            <PencilIcon class="h-12 mr-4" />
+            <div class="flex-row">
+                <h2 class="text-xl font-bold">Advanced Entry</h2>
+                <p class="text-sm">Manually enter/configure a new authentication code.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { QrcodeIcon, LockClosedIcon, PencilIcon } from "@heroicons/vue/outline";
+
+export default defineComponent({
+    name: "New",
+    components: {
+        QrcodeIcon,
+        LockClosedIcon,
+        PencilIcon
+    }
+})
+</script>
