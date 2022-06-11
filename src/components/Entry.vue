7b48db3dce48e542fce5d10a678cd94ed924f83d
Jack <jack@virki.io>
2022-06-11 19:51:54 +0100

    Determine how long a token has left when the entry component is mounted

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="flex items-center justify-center">
-        <div class="flex items-center justify-between w-full h-24 text-white bg-[#214974] rounded-md shadow-md" @click="showTOTP">
+        <div class="flex items-center justify-between w-full h-24 text-white bg-[#214974] rounded-md shadow-md"
+            @click="showTOTP">
             <div class="flex flex-col p-4">
                 <span class="text-xl text-white">{{ issuer }} </span>
                 <p class="text-sm text-gray-200 font-semibold">{{ account }}</p>
+                <p>{{ countdownLeft }}s left</p>
+                <p>{{ generatedCode }}</p>
             </div>
-            <img class="h-full py-2 pr-4 ml-8 h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" />
+            <img class="h-full py-2 pr-4 ml-8 h-16"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" />
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import * as OTPAuth from 'otpauth';
 
 export default defineComponent({
@@ -24,20 +28,63 @@ export default defineComponent({
     },
     setup(props) {
         // The entry component should handle the rendering and countdown of TOTP codes
+        const timestamp = ref(Math.floor(Date.now()));
         const generatedCode = ref();
+        const countdownLeft = ref(30); // Assume that 30 second countdown is left
 
         // Create and return TOTP code from the "secret" prop
         const totp = new OTPAuth.TOTP({
             secret: props.secret,
-            period: 30
+            digits: 6
         })
 
-        // Every second, get the device timestamp and generate the TOTP code
-        setInterval(() => {
-            generatedCode.value = totp.generate();
+        onMounted(() => {
+            // Set the current token based on the timestamp
+            generatedCode.value = totp.generate({ timestamp: timestamp.value })
 
-            console.log(totp.validate({ token: totp.generate() }))
-        }, 1000);
+            // Do a check to see how long the token has left.
+            // We can do this by adding 1 millisecond to the current timestamp until we get
+            // a different token to the one currently stored.
+            for (let i = 0; i < timestamp.value; i++) {
+                // Generate a new token with a different timestamp (incremented by 1ms)
+                const newTimestamp = timestamp.value + i;
+                const newToken = totp.generate({ timestamp: newTimestamp });
+
+                // If the newToken is different to the current token, then
+                // we can set the countdown value to whatever the value of 'i / 1000' is.
+                if (newToken !== generatedCode.value) {
+                    const secondsLeft = Math.floor(i / 1000);
+                    countdownLeft.value = secondsLeft 
+                    break;
+                }
+            }
+
+            // Now that we've worked out the different, we can kick off the normal
+            // interval that handles the countdown
+            // Every second, get the device timestamp and generate the TOTP code
+            setInterval(() => {
+                const timestamp = Math.floor(Date.now());
+                const token = totp.generate({ timestamp });
+
+                // If the token is different to what we've got currently stored
+                // then reset the timer
+                if (token !== generatedCode.value) {
+                    countdownLeft.value = 30;
+                    generatedCode.value = token;
+                    return;
+                }
+
+                // More of a thing for user experience, just don't let the countdown get to zero,
+                // just skip this interval.
+                if (countdownLeft.value -1 == 0) {
+                    return;
+                } else {
+                    countdownLeft.value -= 1;
+                }
+            }, 1000);
+        })
+
+
 
         // Alert user of TOTP
         const showTOTP = () => {
@@ -46,6 +93,7 @@ export default defineComponent({
 
         return {
             generatedCode,
+            countdownLeft,
             showTOTP
         }
     }
