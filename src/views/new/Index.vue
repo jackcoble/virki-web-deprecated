ee1c9b3ee13311bd393b5df2becb599d711b9dfb
Jack <jack@virki.io>
2022-06-11 18:12:28 +0100

    Adjust order and icon sizes

--- src/views/new/Index.vue
+++ src/views/new/Index.vue
@@ -4,19 +4,10 @@
 
         <!-- Scan QR Code -->
         <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
-            <QrcodeIcon class="h-12 mr-4"></QrcodeIcon>
+            <QrcodeIcon class="h-16 mr-4"></QrcodeIcon>
             <div class="flex-row">
-                <h2 class="text-xl font-bold">Scan</h2>
+                <h2 class="text-xl font-bold">Quick Entry</h2>
                 <p class="text-sm">If you have a QR code of your 2FA, you can quickly scan it in here.</p>
-            </div>
-        </div>
-
-        <!-- Create token vault -->
-        <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
-            <LockClosedIcon class="h-12 mr-4" />
-            <div class="flex-row">
-                <h2 class="text-xl font-bold">Vault</h2>
-                <p class="text-sm">Create a new token vault to store your authentication codes.</p>
             </div>
         </div>
 
@@ -28,6 +19,15 @@
                 <p class="text-sm">Manually enter/configure a new authentication code.</p>
             </div>
         </div>
+
+        <!-- Create token vault -->
+        <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
+            <LockClosedIcon class="h-12 mr-4" />
+            <div class="flex-row">
+                <h2 class="text-xl font-bold">Vault</h2>
+                <p class="text-sm">Create a new token vault to store your authentication codes.</p>
+            </div>
+        </div>        
     </div>
 </template>
 
