d937283e6c06462dd2566aa9ce9111eb6da57a69
Jack <jack@virki.io>
2022-06-11 20:39:08 +0100

    Adjust icon sizes on "new" page

--- src/views/new/Index.vue
+++ src/views/new/Index.vue
@@ -4,7 +4,7 @@
 
         <!-- Scan QR Code -->
         <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
-            <QrcodeIcon class="h-16 mr-4"></QrcodeIcon>
+            <QrcodeIcon class="w-16 mr-4"></QrcodeIcon>
             <div class="flex-row">
                 <h2 class="text-xl font-bold">Quick Entry</h2>
                 <p class="text-sm">If you have a QR code of your 2FA, you can quickly scan it in here.</p>
@@ -13,7 +13,7 @@
 
         <!-- Advanced entry -->
         <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
-            <PencilIcon class="h-12 mr-4" />
+            <PencilIcon class="w-12 mr-4" />
             <div class="flex-row">
                 <h2 class="text-xl font-bold">Advanced Entry</h2>
                 <p class="text-sm">Manually enter/configure a new authentication code.</p>
@@ -22,7 +22,7 @@
 
         <!-- Create token vault -->
         <div class="flex items-center container mx-auto p-4 rounded bg-gray-200 text-gray-700">
-            <LockClosedIcon class="h-12 mr-4" />
+            <LockClosedIcon class="w-12 mr-4" />
             <div class="flex-row">
                 <h2 class="text-xl font-bold">Vault</h2>
                 <p class="text-sm">Create a new token vault to store your authentication codes.</p>
