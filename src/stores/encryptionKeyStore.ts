89713c3e837dcfbd4600443828712370c3b55f79
Jack <jack@virki.io>
2022-07-09 21:22:53 +0100

    Repurpose Pinia store for Encryption keys

---
+++ src/stores/encryptionKeyStore.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useEncryptionKeyStore = defineStore({
+  id: 'encryptionKeys',
+  state: () => ({
+    masterKey: ''
+  }),
+  getters: {
+    getMasterKey: (state) => state.masterKey
+  },
+  actions: {
+    setMasterKey (masterKey: string) {
+      this.masterKey = masterKey;
+    },
+
+    clearEncryptionKeys() {
+      this.masterKey = '';
+    }
+  },
+})
