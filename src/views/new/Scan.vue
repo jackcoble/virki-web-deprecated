f2d94a62d65f0fac32812dff626aad63d7f0d4e3
Jack <jack@virki.io>
2022-07-10 14:26:49 +0100

    QR code scanner page

---
+++ src/views/new/Scan.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-screen w-full">
+        <qrcode-stream @decode="onDecode"></qrcode-stream>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Scan",
+    setup() {
+        const onDecode = (event: any) => {
+            alert(event);
+        }
+
+        return {
+            onDecode
+        }
+    }
+})
+</script>
