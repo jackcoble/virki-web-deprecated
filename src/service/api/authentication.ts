5e63bc08de704a3c1fc0b16ab9c0dde854737bde
Jack <jack@virki.io>
2022-07-10 13:48:44 +0100

    Use IRegisterAccount type rather than "any" in API service

--- src/service/api/authentication.ts
+++ src/service/api/authentication.ts
@@ -1,8 +1,10 @@
 import { api } from "@/service/api/api";
+
 import type { AxiosResponse } from "axios";
+import type { IRegisterAccount } from "@/models/account";
 
 export default {
-    RegisterAccount(payload: any): Promise<AxiosResponse> {
+    RegisterAccount(payload: IRegisterAccount): Promise<AxiosResponse> {
         return api.post("/v1/auth/register", payload)
     }
 }
