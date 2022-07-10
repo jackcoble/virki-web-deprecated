3f2a252139a3de4b49a565795838661409c64e67
Jack <jack@virki.io>
2022-07-10 21:19:46 +0100

    Set lastActive timestamp whenever area is clicked

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Tabs from './components/Tabs.vue'
+
+import { getUnixTime } from "date-fns";
+
+// Listen for clicks and update the "lastActive" timestamp
+document.addEventListener("click", () => {
+  const currentDate = new Date();
+  const activeTimestamp = getUnixTime(currentDate).toString();
+
+  localStorage.setItem("lastActive", activeTimestamp)
+})
 </script>
 
 <template>
