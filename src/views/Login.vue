a3d88efae07de4d7a86be8d4fc7e22758d7165b2
Jack <jack@virki.io>
2022-07-10 20:18:00 +0100

    Start working on login screen functionality

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="flex justify-center items-center h-screen">
-        <form @submit.prevent>
+        <form @submit.prevent="handleSignIn">
             <div class="mb-6">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email
                     address</label>
                 <input type="email" id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Email address" required autofocus>
+                    placeholder="Email address" v-model="email" required autofocus>
             </div>
             <div class="mb-6">
                 <label for="password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
                 <input type="password" id="password" placeholder="●●●●●●●●●●●●●●"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required>
+                    required v-model="password">
             </div>
             <div class="flex items-start mb-6">
                 <div class="flex items-center h-5">
                     <input id="remember" type="checkbox" value=""
-                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                        required>
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                 </div>
                 <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
                     email</label>
@@ -31,12 +30,44 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { Account } from "@/class/account";
+import authentication from "@/service/api/authentication";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
     name: "Login",
     setup() {
-        console.log("Hello");
+        const email = ref("");
+        const password = ref("");
+
+        const account = new Account();
+
+        // Handle user login
+        const handleSignIn = async () => {
+            // Using the email address, request carry out a pre-login check for salt.
+            // If we can, extend the password using it
+            try {
+                const res = await authentication.PreLogin(email.value);
+                const saltBuffer = Buffer.from(res.data.password_salt, "base64");
+                const extended = await account.deriveStretchedPassword(password.value, saltBuffer);
+                
+                // Using the extended key we can generate a SHA-256 hash of it to send to the login endpoint.
+                const stretchedKeyBytes = new TextEncoder().encode(extended.key);
+                const stretchedKeyHashBytes = await window.crypto.subtle.digest("SHA-256", stretchedKeyBytes);
+                const stretchedKeyHashEncoded = Buffer.from(stretchedKeyHashBytes).toString("base64");
+
+                await authentication.Login(email.value, stretchedKeyHashEncoded);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        return {
+            email,
+            password,
+
+            handleSignIn
+        }
     }
 })
 </script>
