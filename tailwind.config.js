8fa8185100da7835e877948114e2318c403b97d7
Jack <jack@virki.io>
2022-07-10 21:56:23 +0100

    Add "animate-reverse-spin" animation to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Urbanist", "sans-serif"],
+      },
+      
+      animation: {
+        'reverse-spin': 'reverse-spin 1s linear infinite'
+      },
+
+      keyframes: {
+        'reverse-spin': {
+          from: {
+            transform: 'rotate(360deg)'
+          }
+        }
       }
     },
   },
