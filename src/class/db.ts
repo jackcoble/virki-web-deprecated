94fcdce58fae02afe557df86e5c023157070724d
Jack <jack@virki.io>
2022-07-10 10:36:03 +0100

    Class to handle offline storage of encrypted keys

---
+++ src/class/db.ts
@@ -0,0 +1,21 @@
+import Dexie, { type Table } from "dexie";
+
+// Interface for Dexie table
+interface IAccountDB {
+    account: string;
+    encryptedMasterKey: string;
+}
+
+export class AuthoriserDB extends Dexie {
+    accounts!: Table<IAccountDB>;
+
+    /**
+     * Initialise encrypted key store for multiple accounts
+     */
+    constructor() {
+        super("authoriser");
+        this.version(1).stores({
+            accounts: "account, encryptedMasterKey"
+        })
+    }
+}
