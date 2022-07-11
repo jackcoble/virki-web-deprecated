aea1f331e352f9d6ebf6f28bf5e1ee21894b91e5
Jack <jack@virki.io>
2022-07-11 21:33:53 +0100

    authenticationStore should store refresh token

--- src/stores/authenticationStore.ts
+++ src/stores/authenticationStore.ts
@@ -3,15 +3,22 @@ import { defineStore } from 'pinia'
 export const useAuthenticationStore = defineStore({
   id: 'authentication',
   state: () => ({
-    access_token: localStorage.getItem("access_token") || null
+    access_token: localStorage.getItem("access_token") || null,
+    refresh_token: localStorage.getItem("refresh_token") || null
   }),
   getters: {
-    getAccessToken: (state) => state.access_token
+    getAccessToken: (state) => state.access_token,
+    getRefreshToken: (state) => state.refresh_token
   },
   actions: {
     setAccessToken(accessToken: string) {
       this.access_token = accessToken;
       localStorage.setItem("access_token", accessToken);
+    },
+
+    setRefreshToken(refreshToken: string) {
+        this.refresh_token = refreshToken;
+        localStorage.setItem("refresh_token", refreshToken)
     }
   },
 })
