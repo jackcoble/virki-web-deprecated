ac5be0bb304ceb9cbe93c33529788f868baf19a6
Jack <jack@virki.io>
2022-07-12 20:56:42 +0100

    Add description to settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,6 +2,9 @@
     <div class="h-screen p-4 bg-[#262727] space-y-3">
         <h2 class="text-2xl font-semibold text-white">Settings</h2>
 
+        <!-- Description text -->
+        <p class="text-sm text-white">Customise the settings for your Authoriser account. Some of the options here are disastrous, so watch out! 💥</p>
+
         <!-- Deauthorise sessions -->
         <DeauthoriseModal :show="showDeauthoriseSessionModal" @close="showDeauthoriseSessionModal = false" />
         <button class="p-3 w-full rounded bg-red-400 text-red-900 hover:bg-red-300 hover:text-red-700" @click="showDeauthoriseSessionModal = !showDeauthoriseSessionModal">
