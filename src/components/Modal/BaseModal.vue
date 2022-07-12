a60aa82fb49463f2684ae3ae05b31083e0919db9
Jack <jack@virki.io>
2022-07-12 19:47:34 +0100

    Modal should be vertically centered on screen

--- src/components/Modal/BaseModal.vue
+++ src/components/Modal/BaseModal.vue
@@ -16,7 +16,7 @@
         v-if="showModal"
       >
         <div
-          class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+          class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         >
           <div
             @click="closeModal"
