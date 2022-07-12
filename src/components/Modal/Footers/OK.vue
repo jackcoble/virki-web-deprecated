5c7236275e206a45a969fd84255031a56af04e9a
Jack <jack@virki.io>
2022-07-12 19:54:55 +0100

    Fix flex alignment for OK footer

--- src/components/Modal/Footers/OK.vue
+++ src/components/Modal/Footers/OK.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-    <button
-      @click.prevent="ok"
-      type="button"
-      class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-400 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
-    >
-      OK
-    </button>
+    <div class="mt-5 sm:mt-4 flex items-end space-y-2 space-x-2">
+        <button @click.prevent="ok" type="button"
+            class="w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-400 focus:outline-none sm:text-sm">
+            OK
+        </button>
 
-    <button
-      @click.prevent="cancel"
-      type="button"
-      class="w-full inline-flex justify-center rounded-md border shadow-sm px-4 py-2 text-base font-medium text-gray-900 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
-    >
-      Cancel
-    </button>
-  </div>
+        <button @click.prevent="cancel" type="button"
+            class="w-full rounded-md border shadow-sm px-4 py-2 text-base font-medium text-gray-900 focus:outline-none sm:text-sm">
+            Cancel
+        </button>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup(props, { emit }) {
-    const ok = () => {
-      emit("ok");
-    };
+    setup(props, { emit }) {
+        const ok = () => {
+            emit("ok");
+        };
 
-    const cancel = () => {
-      emit("cancel");
-    };
+        const cancel = () => {
+            emit("cancel");
+        };
 
-    return {
-      ok,
-      cancel,
-    };
-  },
+        return {
+            ok,
+            cancel,
+        };
+    },
 });
 </script>
