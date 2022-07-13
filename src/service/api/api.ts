727d2ec967191019d920b59c75c695b9c0e046bb
Jack <jack@virki.io>
2022-07-13 18:52:38 +0100

    Axios interceptor to handle refreshing access token

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -1,5 +1,6 @@
 import { useAuthenticationStore } from "@/stores/authenticationStore";
 import axios from "axios";
+import authentication from "./authentication";
 
 export const api = axios.create({
     baseURL: "http://localhost:5000",
@@ -22,6 +23,51 @@ api.interceptors.request.use(
         return config;
     },
     error => {
+        return Promise.reject(error);
+    }
+)
+
+// Handle 401 Unauthorised response (access token expired)
+api.interceptors.response.use(
+    response => {
+        return response;
+    },
+
+    async error => {
+        const authenticationStore = useAuthenticationStore();
+        const originalRequest = error.config;
+
+        // Handle the unauthroised response if it isn't a retry attempt
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            // Using the store, fetch current access token and refresh token
+            const accessToken = authenticationStore.getAccessToken;
+            const refreshToken = authenticationStore.getRefreshToken;
+
+            // Submit both credentials to the API endpoint, expecting a new pair to be returned
+            if (accessToken && refreshToken) {
+                try {
+                    const res = await authentication.RefreshTokens(accessToken, refreshToken);
+
+                    authenticationStore.setAccessToken(res.data.access_token);
+                    authenticationStore.setRefreshToken(res.data.refresh_token);
+                } catch (e) {
+                    console.log("Handle this error:", e);
+                    return Promise.reject(e);
+                }
+            }
+
+            else {
+                // TODO: This isn't right, so just force logout and clear all data.
+                return Promise.reject("No access or refresh token found.")
+            }
+
+            // If we have made it this far, update the Authorization header on the original request
+            originalRequest.headers.Authorization = `Bearer ${authenticationStore.getAccessToken}`
+            return axios(originalRequest);
+        }
+
         return Promise.reject(error);
     }
 )
