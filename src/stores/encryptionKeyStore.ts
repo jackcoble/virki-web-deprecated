3e347c353222fceaa7795a6039e12c09b4207929
Jack <jack@virki.io>
2022-07-13 19:28:59 +0100

    Update encryption key store to keep stretched password in LocalStorage and/or memory

--- src/stores/encryptionKeyStore.ts
+++ src/stores/encryptionKeyStore.ts
@@ -3,14 +3,27 @@ import { defineStore } from 'pinia'
 export const useEncryptionKeyStore = defineStore({
   id: 'encryptionKeys',
   state: () => ({
+    masterPasswordStretched: localStorage.getItem("stretched_password") || "",
     masterKey: ""
   }),
   getters: {
-    getMasterKey: (state) => state.masterKey
+    getMasterKey: (state) => state.masterKey,
+    getStretchedPassword: (state) => state.masterPasswordStretched
   },
   actions: {
     setMasterKey (masterKey: string) {
       this.masterKey = masterKey;
+    },
+
+    // Use this for keeping stretched password just in memory for this session
+    setStretchedPassword(password: string) {
+      this.masterPasswordStretched = password;
+    },
+
+    // Use this for persisting stretched password
+    memoriseStretchedPassword(password: string) {
+      this.masterPasswordStretched = password;
+      localStorage.setItem("stretched_password", password);
     },
 
     clear() {
