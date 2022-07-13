2845956d8af4db70825e2c78002e635199cba963
Jack <jack@virki.io>
2022-07-13 22:21:16 +0100

    Autofocus password entry form on Lock screen

--- src/views/Lock.vue
+++ src/views/Lock.vue
@@ -8,7 +8,7 @@
             <!-- Master password entry form -->
             <form @submit.prevent="unlockVault">
                 <!-- Password -->
-                <input type="password" placeholder="Master password" v-model="password" required
+                <input type="password" placeholder="Master password" v-model="password" required autofocus
                     class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
 
                 <!-- User currently logged in -->
