9d5f7d149b71c7622d1baa1f065856dedb03d9cc
Jack <jack@virki.io>
2022-07-13 19:49:44 +0100

    Show signed in email address on vault lock page

--- src/views/Lock.vue
+++ src/views/Lock.vue
@@ -1,11 +1,23 @@
 <template>
     <h1 class="text-xl p-4">Locked!</h1>
+    <p class="text-sm">Currently logged in as: {{ email }}</p>
 </template>
 
 <script lang="ts">
+import { useAuthenticationStore } from "@/stores/authenticationStore";
+import { computed } from "@vue/reactivity";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "Lock"
+    name: "Lock",
+    setup() {
+        const authenticationStore = useAuthenticationStore();
+
+        const email = computed(() => authenticationStore.getEmail);
+
+        return {
+            email
+        }
+    }
 })
 </script>
