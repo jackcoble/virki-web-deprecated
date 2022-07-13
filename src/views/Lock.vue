4b837b97dbe004600b11f0e6e4c1e1b972c39d17
Jack <jack@virki.io>
2022-07-13 22:59:33 +0100

    Use password input component on Lock screen

--- src/views/Lock.vue
+++ src/views/Lock.vue
@@ -8,8 +8,7 @@
             <!-- Master password entry form -->
             <form @submit.prevent="unlockVault" class="space-y-2">
                 <!-- Password -->
-                <input type="password" placeholder="Master password" v-model="password" required autofocus
-                    class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <b-password-input v-model="password" />
 
                 <!-- User currently logged in -->
                 <p class="text-sm">
