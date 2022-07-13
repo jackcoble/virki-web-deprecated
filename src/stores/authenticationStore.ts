434e207a8cf2cb0cc50e0edfedc48cc3e32a533b
Jack <jack@virki.io>
2022-07-13 19:38:54 +0100

    Store email in authentication store

--- src/stores/authenticationStore.ts
+++ src/stores/authenticationStore.ts
@@ -4,16 +4,23 @@ import { getUnixTime } from "date-fns";
 export const useAuthenticationStore = defineStore({
   id: 'authentication',
   state: () => ({
+    email: localStorage.getItem("email") || null,
     access_token: localStorage.getItem("access_token") || null,
     refresh_token: localStorage.getItem("refresh_token") || null,
     lastActiveTimestamp: getUnixTime(new Date())
   }),
   getters: {
+    getEmail: (state) => state.email,
     getAccessToken: (state) => state.access_token,
     getRefreshToken: (state) => state.refresh_token,
     getLastActiveTimestamp: (state) => state.lastActiveTimestamp
   },
   actions: {
+    setEmail(email: string) {
+        this.email = email;
+        localStorage.setItem("email", email);
+    },
+
     setAccessToken(accessToken: string) {
       this.access_token = accessToken;
       localStorage.setItem("access_token", accessToken);
@@ -29,9 +36,11 @@ export const useAuthenticationStore = defineStore({
     },
 
     clear() {
+        this.email = null;
         this.access_token = null;
         this.refresh_token = null;
 
+        localStorage.removeItem("email");
         localStorage.removeItem("access_token");
         localStorage.removeItem("refresh_token");
     }
