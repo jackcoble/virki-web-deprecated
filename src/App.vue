aaae5f989dc2bd33680180af41766336c91777a3
Jack <jack@virki.io>
2022-07-13 19:49:32 +0100

    Fine-tune the vault locking mechanism

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute, useRouter } from 'vue-router'
 import Tabs from './components/Tabs.vue'
 
 import { fromUnixTime, getUnixTime, sub } from "date-fns";
 import { useAuthenticationStore } from './stores/authenticationStore';
+import { onMounted, onUnmounted } from 'vue';
 
+const router = useRouter();
+const currentRoute = useRoute();
 const authenticationStore = useAuthenticationStore();
 
 // Listen for clicks and update the "last active" timestamp in store
@@ -13,18 +16,28 @@ document.addEventListener("click", () => {
   authenticationStore.setLastActiveTimestamp(activeTimestamp);
 })
 
-// Create an interval to check that page hasn't been inactive for the past 30 minutes
-const inactivityInternal = setInterval(() => {
-  const inactivityLimit = 30;
-  const currentDate = new Date();
+let inactivityInterval: NodeJS.Timer;
+onMounted(() => {
+  // Create an interval to check that page hasn't been inactive for the past 30 minutes
+  inactivityInterval = setInterval(() => {
+    const inactivityLimit = 30;
+    const currentDate = new Date();
 
-  const leastActivityDate = sub(currentDate, { minutes: inactivityLimit });
-  if (leastActivityDate > fromUnixTime(authenticationStore.getLastActiveTimestamp)) {
-    console.log("Lock vault!");
-    
-    clearInterval(inactivityInternal);
-  }
-}, 1000)
+    const leastActivityDate = sub(currentDate, { minutes: inactivityLimit });
+    if (leastActivityDate > fromUnixTime(authenticationStore.getLastActiveTimestamp)) {
+      // TODO: Clear keys state
+
+      // Push to locked page if we aren't already there
+      if (currentRoute.path !== '/lock') {
+        router.push("/lock");
+      }
+    }
+  }, 1000);
+
+  onUnmounted(() => {
+    clearInterval(inactivityInterval)
+  })
+})
 </script>
 
 <template>
