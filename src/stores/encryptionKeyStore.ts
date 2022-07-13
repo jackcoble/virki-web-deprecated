89b27d98e9b3e09a164e3b97e3092cd9974cd817
Jack <jack@virki.io>
2022-07-13 19:33:04 +0100

    Store encrypted master key in encryption store

--- src/stores/encryptionKeyStore.ts
+++ src/stores/encryptionKeyStore.ts
@@ -4,11 +4,13 @@ export const useEncryptionKeyStore = defineStore({
   id: 'encryptionKeys',
   state: () => ({
     masterPasswordStretched: localStorage.getItem("stretched_password") || "",
+    encryptedMasterKey: localStorage.getItem("encrypted_master_key") || "",
     masterKey: ""
   }),
   getters: {
     getMasterKey: (state) => state.masterKey,
-    getStretchedPassword: (state) => state.masterPasswordStretched
+    getStretchedPassword: (state) => state.masterPasswordStretched,
+    getEncryptedMasterKey: (state) => state.encryptedMasterKey
   },
   actions: {
     setMasterKey (masterKey: string) {
@@ -23,6 +25,12 @@ export const useEncryptionKeyStore = defineStore({
       if (persist) {
         localStorage.setItem("stretched_password", password);
       }
+    },
+
+    // Use this for settings the users encrypted master key in localStorage
+    setEncryptedMasterKey(encryptedMasterKey: string) {
+      this.encryptedMasterKey = encryptedMasterKey;
+      localStorage.setItem("encrypted_master_key", encryptedMasterKey);
     },
 
     clear() {
