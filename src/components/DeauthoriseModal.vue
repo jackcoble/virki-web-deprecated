5983307a040374fe4df9575c6903ad48f598ad11
Jack <jack@virki.io>
2022-07-13 23:09:47 +0100

    Use custom password input on de-authorise sessions modal

--- src/components/DeauthoriseModal.vue
+++ src/components/DeauthoriseModal.vue
@@ -9,9 +9,7 @@
 
             <!-- Password -->
             <p class="font-bold text-sm">Master Password</p>
-            <input type="password"
-                v-model="password"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <b-password-input v-model="password" />
         </div>
     </BaseModal>
 </template>
