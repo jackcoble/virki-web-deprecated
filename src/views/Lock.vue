22b2592ab523137503d5ed9c3d8fe6e4572795c7
Jack <jack@virki.io>
2022-07-13 21:28:54 +0100

    Re-design of locked page

--- src/views/Lock.vue
+++ src/views/Lock.vue
@@ -1,17 +1,34 @@
 <template>
-    <h1 class="text-xl p-4">Locked!</h1>
-    <p class="text-sm">Currently logged in as: {{ email }}</p>
+    <div class="flex items-center justify-center p-8 h-screen">
+        <div class="p-8 rounded bg-gray-50 border border-gray-200 space-y-2">
+            <!-- Header -->
+            <LockClosedIcon class="h-24 text-gray-700 p-2 mx-auto" />
+            <h1 class="text-lg text-center">Authoriser is locked. Verify your password to continue.</h1>
 
-    <!-- Input to unlock -->
-    <!-- Password -->
-    <p class="font-bold text-sm">Master Password</p>
-    <input type="password" v-model="password"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <!-- Password -->
+            <input type="password" placeholder="Master password" v-model="password"
+                class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
 
-    <!-- Unlock button -->
-    <button class="p-3 w-full rounded bg-blue-400 text-blue-900 hover:bg-blue-300 hover:text-blue-700" @click="unlockVault">
-            Unlock
-        </button>
+            <!-- User currently logged in -->
+            <p class="text-sm">
+                Currently signed in as
+                <span class="font-bold">{{ email }}</span>
+            </p>
+
+            <!-- Unlock and Logout buttons -->
+            <div class="mt-5 sm:mt-4 flex items-end space-y-2 space-x-2">
+                <button @click.prevent="unlockVault" type="button"
+                    class="w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-400 focus:outline-none sm:text-sm">
+                    Unlock
+                </button>
+
+                <button @click.prevent="logoutUser" type="button"
+                    class="w-full rounded-md border shadow-sm px-4 py-2 text-base font-medium text-gray-900 focus:outline-none sm:text-sm">
+                    Logout
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -23,9 +40,13 @@ import { useEncryptionKeyStore } from "@/stores/encryptionKeyStore";
 import { computed } from "@vue/reactivity";
 import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
+import { LockClosedIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
     name: "Lock",
+    components: {
+        LockClosedIcon
+    },
     setup() {
         const router = useRouter();
         const toaster = useToaster();
@@ -71,11 +92,22 @@ export default defineComponent({
             router.push("/")
         }
 
+        // Function to log out user
+        const logoutUser = () => {
+            // Clear all stores
+            authenticationStore.clear();
+            encryptionKeyStore.clear();
+
+            // Push to login
+            router.push("/login");
+        }
+
         return {
             email,
             password,
 
-            unlockVault
+            unlockVault,
+            logoutUser
         }
     }
 })
