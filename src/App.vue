89c2b22758bfbc6a4ca79cca707cd9783ac54e04
Jack <jack@virki.io>
2022-07-13 19:07:16 +0100

    Interval that should trigger a future locking mechanism

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView } from 'vue-router'
 import Tabs from './components/Tabs.vue'
 
-import { getUnixTime } from "date-fns";
+import { fromUnixTime, getUnixTime, sub } from "date-fns";
 import { useAuthenticationStore } from './stores/authenticationStore';
 
 const authenticationStore = useAuthenticationStore();
@@ -12,6 +12,19 @@ document.addEventListener("click", () => {
   const activeTimestamp = getUnixTime(new Date());
   authenticationStore.setLastActiveTimestamp(activeTimestamp);
 })
+
+// Create an interval to check that page hasn't been inactive for the past 30 minutes
+const inactivityInternal = setInterval(() => {
+  const inactivityLimit = 30;
+  const currentDate = new Date();
+
+  const leastActivityDate = sub(currentDate, { minutes: inactivityLimit });
+  if (leastActivityDate > fromUnixTime(authenticationStore.getLastActiveTimestamp)) {
+    console.log("Lock vault!");
+    
+    clearInterval(inactivityInternal);
+  }
+}, 1000)
 </script>
 
 <template>
