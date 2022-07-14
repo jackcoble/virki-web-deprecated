6b55c2acf06fddf80347c03fb4e3584c8ca25f1b
Jack <jack@virki.io>
2022-07-14 19:19:30 +0100

    Add options to account settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,21 +1,62 @@
 <template>
-    <div class="h-screen p-4 bg-[#262727] space-y-3">
-        <h2 class="text-2xl font-semibold text-white">Settings</h2>
+    <div class="h-screen bg-gray-50">
+        <div class="container p-4 mx-auto space-y-3">
+            <h2 class="text-2xl font-semibold text-gray-900">Settings ⚙️</h2>
 
-        <!-- Description text -->
-        <p class="text-sm text-white">Customise the settings for your Authoriser account. Some of the options here are disastrous, so watch out! 💥</p>
+            <!-- Explanation text -->
+            <p class="text-sm text-gray-700">Here you can make changes to your Authoriser account settings. Beware that
+                some of these settings only take affect on this device.</p>
+            <div class="w-full border-t-2 border-gray-300"></div>
 
-        <!-- Deauthorise sessions -->
-        <DeauthoriseModal :show="showDeauthoriseSessionModal" @close="showDeauthoriseSessionModal = false" />
-        <button class="p-3 w-full rounded bg-red-400 text-red-900 hover:bg-red-300 hover:text-red-700" @click="showDeauthoriseSessionModal = !showDeauthoriseSessionModal">
-            De-authorise sessions
-        </button>
-              
+            <div class="space-y-2">
+                <!-- Account settings -->
+                <router-link to="/" class="flex flex-row items-middle p-3 border rounded">
+                    <UserIcon class="w-5 mr-1" />
+                    <span class="text-sm font-medium">My Account</span>
+                </router-link>
+
+                <!-- Security settings -->
+                <router-link to="/" class="flex flex-row items-middle p-3 border rounded">
+                    <ShieldCheckIcon class="w-5 mr-1" />
+                    <span class="text-sm font-medium">Security</span>
+                </router-link>
+
+                <!-- Preferences -->
+                <router-link to="/" class="flex flex-row items-middle p-3 border rounded">
+                    <AdjustmentsIcon class="w-5 mr-1" />
+                    <span class="text-sm font-medium">Preferences</span>
+                </router-link>
+
+                <!-- Subscription -->
+                <router-link to="/" class="flex flex-row items-middle p-3 border rounded">
+                    <StarIcon class="w-5 mr-1" />
+                    <span class="text-sm font-medium">Subscription</span>
+                </router-link>
+            </div>
+
+            <!-- Advanced settings -->
+            <div class="w-full border-t-2 border-gray-300"></div>
+            <p class="text-xs font-medium text-gray-700">Advanced Settings</p>
+
+            <!-- Deauthorise sessions -->
+            <DeauthoriseModal :show="showDeauthoriseSessionModal" @close="showDeauthoriseSessionModal = false" />
+            <b-button classType="danger"
+                @click="showDeauthoriseSessionModal = !showDeauthoriseSessionModal">
+                De-authorise sessions
+            </b-button>
+
+            <!-- Logout current user -->
+            <b-button classType="danger">
+                Logout
+            </b-button>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+
+import { UserIcon, ShieldCheckIcon, AdjustmentsIcon, StarIcon } from "@heroicons/vue/outline"
 
 // Custom components
 import DeauthoriseModal from "@/components/DeauthoriseModal.vue";
@@ -23,7 +64,12 @@ import DeauthoriseModal from "@/components/DeauthoriseModal.vue";
 export default defineComponent({
     name: "Settings",
     components: {
-        DeauthoriseModal
+        DeauthoriseModal,
+
+        UserIcon,
+        ShieldCheckIcon,
+        AdjustmentsIcon,
+        StarIcon
     },
     setup() {
         const showDeauthoriseSessionModal = ref(false);
