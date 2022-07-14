b785ac97b691d0554aaec26ef06e16b1d4470c91
Jack <jack@virki.io>
2022-07-14 18:03:56 +0100

    Add password strength meter to password input component (controlled by prop)

--- src/components/PasswordInput.vue
+++ src/components/PasswordInput.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="flex">
-        <input :type="showPassword ? 'text' : 'password'" :value="modelValue" @input="updateValue" :placeholder="placeholder"
-            class="border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2"
-            :class="showPassword && modelValue ? 'font-mono' : 'font-sans'"
-        >
+        <div class="flex-row flex-grow">
+            <input :type="showPassword ? 'text' : 'password'" :value="modelValue" @input="updateValue" :placeholder="placeholder"
+            class="border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2 h-10"
+            :class="showPassword && modelValue ? 'font-mono' : 'font-sans'">
 
-        <button type="button" class="m-2 w-5 text-purple-800" @click="showPassword = !showPassword">
+            <password-strength v-if="showStrength" class="mt-1" :password="modelValue" />
+        </div>
+
+        <div class="flex-col items-center justify-center">
+            <button type="button" class="m-2 w-5 text-purple-800" @click="showPassword = !showPassword">
             <EyeOffIcon v-if="showPassword" />
             <EyeIcon v-if="!showPassword" />
         </button>
+        </div>
     </div>
 </template>
 
@@ -29,6 +34,11 @@ export default defineComponent({
 
         placeholder: {
             type: String
+        },
+
+        showStrength: {
+            type: Boolean,
+            default: false
         }
     },
     setup(props, context) {
