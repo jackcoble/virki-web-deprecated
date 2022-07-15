f41e6c2a30b54d81da5f1155d4701aa9d4f0b16e
Jack <jack@virki.io>
2022-07-15 22:14:57 +0100

    Update vault selection component icon to be grey

--- src/components/VaultSelector.vue
+++ src/components/VaultSelector.vue
@@ -2,7 +2,7 @@
     <!-- Vault currently active -->
     <div class="flex flex-row items-center space-x-2">
         <div class="object-contain cursor-pointer rounded-full w-10" :class="[vaultStore.getActiveVault?.icon ? 'rounded-full' : 'bg-gray-200 p-1.5']" @click="showModal = !showModal">
-            <ClockIcon v-if="!vaultStore.getActiveVault?.icon" class="text-purple-800" />
+            <ClockIcon v-if="!vaultStore.getActiveVault?.icon" class="text-gray-500" />
             <img v-else class="rounded-full" :src="vaultStore.getActiveVault?.icon" alt="Vault Icon">
         </div>
 
