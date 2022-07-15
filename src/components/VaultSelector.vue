2153570394f18b6b15617913cba9bc33159c65dc
Jack <jack@virki.io>
2022-07-15 18:24:03 +0100

    Start of redesigned vault selector component

--- src/components/VaultSelector.vue
+++ src/components/VaultSelector.vue
@@ -1,35 +1,57 @@
 <template>
-  <Menu as="div" class="relative inline-block text-left">
-    <div>
-      <MenuButton class="inline-flex w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700">
-        {{ vaultStore.getVaults.length !== 0 ? vaultStore.getVaults[0].name : 'No vualts :()' }}
-        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-      </MenuButton>
-    </div>
+    <!-- Vault currently active -->
+    <button @click="showModal = !showModal"
+        class="rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+        Personal
+    </button>
 
-    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-      <MenuItems class="origin-top-right absolute left-0 mt-1 w-56 rounded-md overflow-auto shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-        <div>
-          <MenuItem v-slot="{ active }" v-for="vault in vaultStore.getVaults" :key="vault.id">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">{{ vault.name }}</a>
-          </MenuItem>
+    <BaseModal :show="showModal" okFooter>
+        <div class="space-y-2">
+            <h1 class="text-xl text-center">Vault Selector</h1>
+            <p class="text-xs">
+                Quickly switch between the different vaults for your authentication tokens.
+            </p>
 
-          <!-- Create new Vault -->
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm font-medium']">
-                Create new vault
-            </a>
-          </MenuItem>
+            <fieldset class="h-48 w-full p-2 overflow-auto">
+                <div class="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
+                    <div v-for="(vault) in vaults" :key="vault.id" class="relative flex items-start py-3">
+                        <div class="mr-3 flex items-center h-5">
+                            <input type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" :checked="vault.id === activeVaultId" />
+                        </div>
+                        <div class="min-w-0 flex-1 text-sm">
+                            <label class="font-medium text-gray-700 select-none">{{ vault.name }}</label>
+                        </div>
+                    </div>
+                </div>
+            </fieldset>
         </div>
-      </MenuItems>
-    </transition>
-  </Menu>
+    </BaseModal>
 </template>
 
-<script lang="ts" setup>
-import { useVaultStore } from '@/stores/vaultStore';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { ChevronDownIcon } from '@heroicons/vue/solid'
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import BaseModal from "@/components/Modal/BaseModal.vue";
+import { useVaultStore } from "@/stores/vaultStore";
 
-const vaultStore = useVaultStore();
+export default defineComponent({
+    components: {
+        BaseModal,
+    },
+    setup() {
+        const vaultStore = useVaultStore();
+
+        // Ref controlling whether modal should be shown or not
+        const showModal = ref(false);
+
+        const activeVaultId = vaultStore.getActiveVaultID;
+        const vaults = vaultStore.getVaults;
+
+        return {
+            showModal,
+
+            activeVaultId,
+            vaults
+        }
+    },
+});
 </script>
