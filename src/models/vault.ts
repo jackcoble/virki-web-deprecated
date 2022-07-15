104ed67f84bedbbd8a3dec8584d6c4a00b0c321a
Jack <jack@virki.io>
2022-07-15 17:51:10 +0100

    Encrypted vault model could contain an ID

--- src/models/vault.ts
+++ src/models/vault.ts
@@ -1,5 +1,6 @@
 // EncryptedVault is what we are expected in the OpenPGP message after decryption
 interface EncryptedVault {
+    id?: string;
     name: string;
     description: string;
     icon?: string;
