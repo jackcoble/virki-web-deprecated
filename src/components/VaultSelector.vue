028654fb2b25541627c47d807eaddc9d51004610
Jack <jack@virki.io>
2022-07-15 18:50:19 +0100

    Refactoring of vault selector component

--- src/components/VaultSelector.vue
+++ src/components/VaultSelector.vue
@@ -2,7 +2,7 @@
     <!-- Vault currently active -->
     <button @click="showModal = !showModal"
         class="text-left w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
-        {{ activeVault ? activeVault.name : '' }}
+        {{ vaultStore.getActiveVault ? vaultStore.getActiveVault.name : '' }}
     </button>
 
     <BaseModal :show="showModal" @cancel="showModal = !showModal" @close="showModal = !showModal" okFooter>
@@ -14,9 +14,9 @@
 
             <fieldset class="h-48 w-full p-2 overflow-auto">
                 <div class="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
-                    <div v-for="(vault) in vaults" :key="vault.id" class="relative flex items-start py-3">
+                    <div v-for="(vault) in vaultStore.getVaults" :key="vault.id" class="relative flex items-start py-3">
                         <div class="mr-3 flex items-center h-5">
-                            <input type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" :checked="vault.id === activeVault!.id" />
+                            <input type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" :checked="vault.id === vaultStore.getActiveVaultId" />
                         </div>
                         <div class="min-w-0 flex-1 text-sm">
                             <label class="font-medium text-gray-700 select-none">{{ vault.name }}</label>
@@ -29,7 +29,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from "vue";
+import { defineComponent, onMounted, ref, watch } from "vue";
 import BaseModal from "@/components/Modal/BaseModal.vue";
 import { useVaultStore } from "@/stores/vaultStore";
 import type { EncryptedVault } from "@/models/vault";
@@ -43,26 +43,10 @@ export default defineComponent({
 
         // Ref controlling whether modal should be shown or not
         const showModal = ref(false);
-        const activeVault = ref({} as EncryptedVault);
-
-        // Watch the vault store for a change to the active vault ID
-        watch(
-            () => vaultStore.getActiveVaultId,
-            (id, prevId) => {
-                if (id !== prevId) {
-                    // Assume vault has been changed, so update active vault
-                    activeVault.value = vaultStore.getActiveVault!;
-                }
-            }
-        )
-
-        const vaults = vaultStore.getVaults;
 
         return {
             showModal,
-
-            activeVault,
-            vaults
+            vaultStore
         }
     },
 });
