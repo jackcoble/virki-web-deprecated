426bb54cee68456fd2dc5138038299e6e9a47d52
Jack <jack@virki.io>
2022-07-15 18:27:52 +0100

    Maintain state of vault in use (active vault)

--- src/stores/vaultStore.ts
+++ src/stores/vaultStore.ts
@@ -2,16 +2,28 @@ import type { EncryptedVault } from '@/models/vault';
 import { defineStore } from 'pinia'
 
 export const useVaultStore = defineStore({
-  id: 'vaults',
-  state: () => ({
-    vaults: [] as EncryptedVault[] 
-  }),
-  getters: {
-    getVaults: (state) => state.vaults
-  },
-  actions: {
-    add(vault: EncryptedVault) {
-        this.vaults.push(vault);
-    }
-  },
+    id: 'vaults',
+    state: () => ({
+        activeVaultId: "",
+        vaults: [] as EncryptedVault[]
+    }),
+    getters: {
+        getVaults: (state) => state.vaults,
+        getActiveVaultId: (state) => state.activeVaultId,
+        getActiveVault: (state) => {
+            const activeVaultId = state.activeVaultId;
+            const activeVault = state.vaults.find(v => v.id === activeVaultId);
+
+            return activeVault;
+        }
+    },
+    actions: {
+        add(vault: EncryptedVault) {
+            this.vaults.push(vault);
+        },
+
+        setActiveVault(id: string) {
+            this.activeVaultId = id;
+        }
+    },
 })
