3724ac7eb8e3288fc25cfe16d6569051ca43ce48
Jack <jack@virki.io>
2022-07-15 17:47:58 +0100

    Start of Vault selector component

---
+++ src/components/VaultSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton class="inline-flex w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700">
+        {{ vaultStore.getVaults.length !== 0 ? vaultStore.getVaults[0].name : 'No vualts :()' }}
+        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+      </MenuButton>
+    </div>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems class="origin-top-right absolute left-0 mt-1 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
+        <div class="py-1">
+          <MenuItem v-slot="{ active }" v-for="vault in vaultStore.getVaults" :key="vault.id">
+            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">{{ vault.name }}</a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script lang="ts" setup>
+import { useVaultStore } from '@/stores/vaultStore';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/solid'
+
+const vaultStore = useVaultStore();
+</script>
