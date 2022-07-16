b8b2b62fd9d4304091296d1f9944088af2369333
Jack <jack@virki.io>
2022-07-16 10:22:47 +0100

    Improved design for TOTP entry

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -1,19 +1,23 @@
 <template>
-    <div class="pb-2">
-        <div class="flex items-center w-full h-24 text-gray-900" @click="showTOTP">
-            <img class="h-16 pr-4" :src="icon" />
+    <div class="flex p-2 space-x-4">
+        <!-- Issuer icon -->
+        <div class="flex-shrink-0 ">
+            <img class="w-12 h-12 m-2 rounded-full " :src="icon" />
+        </div>
 
-            <div class="flex flex-col flex-grow">
-                <span class="text-sm text-gray-900">{{ issuer }} </span>
-                <p class="text-xs text-gray-700 font-semibold pb-2">{{ account }}</p>
-                <p class="text-2xl">{{ generatedCode }}</p>
-            </div>
+        <!-- Token details -->
+        <div class="md:w-3/4 w-6/12">
+            <p class="text-sm text-gray-900 truncate">{{ issuer }} </p>
+            <p class="text-xs text-gray-700 font-semibold">{{ account }}</p>
+            <p class="text-2xl">{{ generatedCode }}</p>
+        </div>
 
-            <div class="flex-col mr-2">
-                <p class="text-xs">{{ countdownLeft }}s left</p>
-            </div>
+        <!-- Time remaining -->
+        <div class="flex-grow pl-6">
+            <p class="text-xs">{{ countdownLeft }}s left</p>
         </div>
     </div>
+
 </template>
 
 <script lang="ts">
