64efee266cce9e23c2232a7c1126f749bcfa62bc
Jack <jack@virki.io>
2022-07-16 17:34:19 +0100

    Add "update" endpoint to vault service API

--- src/service/api/vault.ts
+++ src/service/api/vault.ts
@@ -1,11 +1,18 @@
 import { api } from "@/service/api/api";
-import type { AxiosResponse } from "axios";
+import type { Axios, AxiosResponse } from "axios";
 
 export default {
     CreateVault(encryptedData: string): Promise<AxiosResponse> {
         return api.post("/v1/vault", {
             data: encryptedData
         });
+    },
+
+    UpdateVault(vaultId: string, encryptedData: string): Promise<AxiosResponse> {
+        return api.put("/v1/vault", {
+            id: vaultId,
+            data: encryptedData
+        })
     },
 
     GetVaults(): Promise<AxiosResponse> {
