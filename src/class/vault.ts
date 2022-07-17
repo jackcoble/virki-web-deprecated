246b2964c6bcd7d19fb856b61d28e33894f3c4a1
Jack <jack@virki.io>
2022-07-17 16:26:39 +0100

    Vault class method to decrypt from object

--- src/class/vault.ts
+++ src/class/vault.ts
@@ -15,6 +15,12 @@ class Vault extends Account {
         super(masterKey);
     }
 
+    /**
+     * Creates an encrypted vault object that can be used to submit to our API.
+     * @param vaultDetails 
+     * @param offline 
+     * @returns 
+     */
     async createEncryptedVaultObject(vaultDetails: IVault, offline?: boolean): Promise<IVault> {
         // Generate a UUID (v4), remove hyphens and prepend 'v' to indicate vault, and append with EncryptionType to
         // indicate the type of encryption we are using.
@@ -51,6 +57,44 @@ class Vault extends Account {
         vaultDetails.created = currentUnixMilliseconds;
 
         return Promise.resolve(vaultDetails);
+    }
+
+    /**
+     * Decrypts an encrypted vault object
+     * @param vault 
+     */
+    async decryptFromVaultObject(vault: IVault): Promise<IVault> {
+        // We first need to parse the v_id (Vault ID) as it contains some information such as the encryption type we want to use.
+        // At the moment we only use OpenPGP.js, but it's good to future-proof ourselves!
+        const splitVaultID = vault.v_id.split("-");
+        const encryptionType = parseInt(splitVaultID[2].substring(1));
+
+        if (encryptionType) {
+            switch (encryptionType) {
+                // Decrypt using OpenPGP.js
+                case EncryptionType.OPENPGP:
+                    // The properties we are mainly interested in are Name, Description and Icon    
+                    vault.name = await this.decryptData(vault.name);
+
+                    if (vault.description) {
+                        vault.description = await this.decryptData(vault.description);
+                    }
+
+                    if (vault.icon) {
+                        vault.icon = await this.decryptData(vault.icon);
+                    }
+
+                    break;
+            
+                // We have no other encryption types, so something went wrong here...
+                default:
+                    return Promise.reject("Encryption type is not supported!");
+            }
+        } else {
+            return Promise.reject("Could not determine encryption type!")
+        }
+
+        return Promise.resolve(vault);
     }
 
     // IndexedDB Methods
