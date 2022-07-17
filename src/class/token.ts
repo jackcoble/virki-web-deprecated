e44e6d9d968427c1da0b2ea082ea3dcd2742957d
Jack <jack@virki.io>
2022-07-17 18:59:00 +0100

    Method to create and return encrypted Token object

--- src/class/token.ts
+++ src/class/token.ts
@@ -1,4 +1,4 @@
-import { Account } from "./account";
+import { Account, EncryptionType } from "./account";
 import * as OTPAuth from 'otpauth';
 
 // Enums for Algorithm and Type
@@ -14,6 +14,11 @@ enum TokenType {
 
 // Interface describing the properties a "Token" entry should have
 interface IToken {
+    t_id: string;
+    offline: boolean | number // If device is offline, this should be a timestamp of when it went offline in UNIX time (ms)
+    created: number;
+
+    // OTP specific data
     issuer: string;
     account: string;
     secret: string;
@@ -36,7 +41,46 @@ class Token extends Account {
      * @param token 
      * @returns {token}
      */
-    async createEncryptedToken(token: IToken): Promise<IToken> {
+    async createEncryptedToken(token: IToken, offline?: boolean): Promise<IToken> {
+        // If we don't have a token ID for this entry, generate one.
+        // It will be in the same format that we use for Vaults.
+        if (!token.t_id) {
+            // Token format: t-d68f86c1fbe145b7bbba0f36b4cdfa6e-v1
+            //               [Token Identifier Character]-[UUID]-[Encryption Type]
+            let tokenId = window.crypto.randomUUID();
+            tokenId = tokenId.replace(/-/g, "");
+            tokenId = `t-${tokenId}-v${EncryptionType.OPENPGP}`;
+
+            token.t_id = tokenId;
+        }
+
+        // We should encrypt the following data:
+        // - Issuer
+        // - Account
+        // - Secret
+        // - Icon (if available)
+        token.issuer = await this.encryptData(token.issuer);
+        token.account = await this.encryptData(token.account);
+        token.secret = await this.encryptData(token.secret);
+
+        if (token.icon) {
+            token.icon.data = await this.encryptData(token.icon.data);
+            token.icon.url = await this.encryptData(token.icon.url);
+        }
+
+        // If device is offline, set the offline timestamp as current device UNIX time (microseconds)
+        const currentUnixMilliseconds = Math.floor(Date.now() * 1000);
+        if (offline === true) {
+            token.offline = currentUnixMilliseconds;
+        } else {
+            token.offline = false;
+        }
+
+        // Set the created time as current UNIX time if not already set
+        if (!token.created) {
+            token.created = currentUnixMilliseconds;
+        }
+        
         return Promise.resolve(token);
     }
 
