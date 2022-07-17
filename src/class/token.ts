c306e6629ff91deac6f86a9e8ba39712ec4171b7
Jack <jack@virki.io>
2022-07-17 18:06:37 +0100

    Start of class to handle Tokens

---
+++ src/class/token.ts
@@ -0,0 +1,50 @@
+import { Account } from "./account";
+
+// Enums for Algorithm and Type
+enum TokenAlgorithm {
+    SHA1 = 0,
+    SHA256,
+    SHA512
+}
+
+enum TokenType {
+    TOTP = 0
+}
+
+// Interface describing the properties a "Token" entry should have
+interface IToken {
+    issuer: string;
+    account: string;
+    secret: string;
+    icon: {
+        url: string;
+        data: string;
+    }
+    algorithm: TokenAlgorithm;
+    type: TokenType;
+}
+
+class Token extends Account {
+    constructor(masterKey: string) {
+        super(masterKey);
+    }
+
+    /**
+     * Encrypt and return a token object
+     * @param token 
+     * @returns {token}
+     */
+    async createEncryptedToken(token: IToken): Promise<IToken> {
+        return Promise.resolve(token);
+    }
+}
+
+export {
+    Token
+}
+
+export type {
+    IToken,
+    TokenAlgorithm,
+    TokenType
+}
