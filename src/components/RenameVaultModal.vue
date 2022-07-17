13871cb011f22b554bf62ff4136500641efa17b2
Jack <jack@virki.io>
2022-07-17 21:35:21 +0100

    RenameVault modal component to update other vault properties

--- src/components/RenameVaultModal.vue
+++ src/components/RenameVaultModal.vue
@@ -1,15 +1,26 @@
 <template>
     <BaseModal :show="show" okFooter @ok="handleRename">
         <template v-slot:body>
-            <div class="space-y-2">
+            <div class="space-y-3">
                 <h1 class="text-xl text-center">Edit your vault</h1>
+                <p class="text-sm text-center">Make changes to your vault name, description and icon.</p>
 
                 <!-- Icon -->
+                <div class="flex justify-center">
+                    <IconUpload :image="activeVault?.icon" @image-data="handleImageData" />
+                </div>
                
                 <!-- Name -->
-                <b-input type="text" v-model="vaultName" :placeholder="activeVaultName" />
-
+                <div class="space-y-1">
+                    <p class="font-bold text-sm">Name</p>
+                    <b-input type="text" v-model="name" :placeholder="activeVaultName" />
+                </div>
+               
                 <!-- Description -->
+                <div class="space-y-1">
+                    <p class="font-bold text-sm">Description</p>
+                    <textarea v-model="description" :placeholder="activeVault?.description" rows="4" placeholder="A description of what you'll store in this vault" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2" />
+                </div>
             </div>
         </template>
     </BaseModal>
@@ -23,12 +34,15 @@ import type { IVault } from "@/class/vault";
 import useAccount from "@/composables/useAccount";
 import useVault from "@/composables/useVault";
 import { useApplicationStore } from "@/stores/appStore";
+import IconUpload from "./IconUpload.vue";
+import { es } from "date-fns/locale";
 
 export default defineComponent({
     name: "RenameVaultModal",
     components: {
-        BaseModal,
-    },
+    BaseModal,
+    IconUpload
+},
     props: {
         show: {
             type: Boolean,
@@ -41,13 +55,17 @@ export default defineComponent({
         const applicationStore = useApplicationStore();
         const vaultStore = useVaultStore();
         
-        const vaultName = ref("");
+        const name = ref("");
+        const description = ref("");
+        const icon = ref("");
+
+        const activeVault = computed(() => vaultStore.getActiveVault);
         const activeVaultName = computed(() => vaultStore.getActiveVault?.name);
 
         // Function to handle re-encryption of vault data with updated name
         const handleRename = async () => {
             // If new vault name is empty, just close the modal
-            if (!vaultName.value || !account) {
+            if (!name.value || !account) {
                 return;
             }
 
@@ -57,8 +75,10 @@ export default defineComponent({
             if (activeVault) {
                 const modifiedVault = { ...activeVault };
 
-                // Update vault name
-                modifiedVault.name = vaultName.value;
+                // Update vault name, description and icon
+                modifiedVault.name = name.value;
+                modifiedVault.description = description.value;
+                modifiedVault.icon = icon.value;
 
                 // Re-encrypt the active vault with the new data, and save it to IndexedDB
                 const encryptedActiveVault = await vault?.createEncryptedVaultObject(modifiedVault, !applicationStore.isOnline);
@@ -70,11 +90,29 @@ export default defineComponent({
             }
         }
 
-        return {
-            vaultName,
-            activeVaultName,
+        // Function to handle the "imageData" event from icon upload component
+        const handleImageData = (e: any) => {
+            // If the event is undefined, that means
+            // the image has been cleared, so do that with our icon
+            if (!e) {
+                icon.value = "";
+            }
+            else {
+                // Update icon from the string in the event
+                icon.value = e;
+            }
+        }
 
-            handleRename
+        return {
+            name,
+            description,
+            icon,
+
+            activeVaultName,
+            activeVault,
+
+            handleRename,
+            handleImageData
         }
     }
 });
