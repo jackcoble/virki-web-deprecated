801f861a08eeab142c634f0cc71899d31947fdff
Jack <jack@virki.io>
2022-07-21 18:09:58 +0100

    Update Vault API service

--- src/service/api/vaultService.ts
+++ src/service/api/vaultService.ts
@@ -1,17 +1,14 @@
-import type { EncryptedVault } from "@/class/vault";
+import type { IVault } from "@/class/vault";
 import { api } from "@/service/api/api";
 import type { Axios, AxiosResponse } from "axios";
 
 export default {
-    CreateVault(vault: EncryptedVault): Promise<AxiosResponse> {
+    CreateVault(vault: IVault): Promise<AxiosResponse> {
         return api.post("/v1/vault", vault);
     },
 
-    UpdateVault(vault: EncryptedVault): Promise<AxiosResponse> {
-        return api.put("/v1/vault", {
-            id: vault.id,
-            data: vault.data
-        })
+    UpdateVault(vault: IVault): Promise<AxiosResponse> {
+        return api.put("/v1/vault", vault);
     },
 
     GetVaults(): Promise<AxiosResponse> {
