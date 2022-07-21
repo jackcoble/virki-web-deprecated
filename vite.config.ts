5c27a438e08be1e521f3522e687a0a08a16fe442
Jack <jack@virki.io>
2022-07-21 18:37:33 +0100

    Remove PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA, type ManifestOptions } from "vite-plugin-pwa"
-
-// PWA Manifest
-const pwa_manifest = {
-  name: "Authoriser",
-  description: "Secure your two-factor authentication tokens with Authoriser.",
-  orientation: "portrait",
-  display: "standalone",
-  start_url: "/"
-} as ManifestOptions;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      manifest: pwa_manifest,
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    vue()
   ],
   resolve: {
     alias: {
