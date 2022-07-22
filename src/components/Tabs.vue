c943dea8dd62ff1c7ad7de5c431e93ba35edabd0
Jack <jack@virki.io>
2022-07-22 23:20:21 +0100

    Update Tabs component

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,8 +1,9 @@
 <template>
-<nav class="fixed bottom-0 p-2 w-full bg-gray-50 flex border-t-2 border-gray-200">
+<nav class="fixed bottom-0 p-1.5 w-full bg-gray-50 flex border-t-2 border-gray-200">
     <div v-for="button in buttons" :key="button.label" class="flex flex-col flex-grow w-full justify-center items-center text-center truncate">
         <router-link :to="button.route">
-            <component class="mx-auto" :class="[button.classes, currentRoute.path === button.route ? 'text-purple-800' : '']" :is="button.icon" />
+            <component class="mx-auto" :class="[currentRoute.path === button.route ? 'text-purple-800' : 'text-gray-400', button.classes]" :is="button.icon" />
+            <span class="text-xs">{{ button.label }}</span>
         </router-link>
     </div>
 </nav>
@@ -10,7 +11,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { HomeIcon, PlusCircleIcon, CogIcon } from "@heroicons/vue/solid";
+import { ClockIcon, CogIcon, TagIcon } from "@heroicons/vue/solid";
 import { useRoute } from 'vue-router';
 
 const currentRoute = useRoute();
@@ -19,20 +20,20 @@ const buttons = ref([
     {
         label: "Tokens",
         route: "/",
-        icon: HomeIcon,
-        classes: "h-6 text-gray-400"
+        icon: ClockIcon,
+        classes: "h-6"
     },
     {
-        label: "New",
-        route: "/new",
-        icon: PlusCircleIcon,
-        classes: "h-12 text-purple-800"
+        label: "Tags",
+        route: "/tags",
+        icon: TagIcon,
+        classes: "h-6"
     },
     {
         label: "Settings",
         route: "/settings",
         icon: CogIcon,
-        classes: "h-6 text-gray-400"
+        classes: "h-6"
     }
 ])
 </script>
