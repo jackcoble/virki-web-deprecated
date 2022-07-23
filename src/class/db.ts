ec5056c253b46cca5f6e4e1e2091df7a56b7681a
Jack <jack@virki.io>
2022-07-23 20:01:32 +0100

    Insert, Get and Remove methods for Vaults in IndexedDB

--- src/class/db.ts
+++ src/class/db.ts
@@ -10,9 +10,9 @@ interface IAccountDB {
 }
 
 class AuthoriserDB extends Dexie {
-    accounts!: Table<IAccountDB>;
-    vaults!: Table<IVault>;
-    tokens!: Table<IToken>
+    private accounts!: Table<IAccountDB>;
+    private vaults!: Table<IVault>;
+    private tokens!: Table<IToken>
 
     /**
      * Initialise encrypted key store for multiple accounts
@@ -24,6 +24,37 @@ class AuthoriserDB extends Dexie {
             vaults: "v_id, name, description, icon, key, modified, created",
             tokens: "t_id, v_id, issuer, account, secret, icon, algorithm, type, duration, digits, offline, created, modified"
         })
+    }
+
+    /**
+     * Insert an encrypted vault into IndexedDB for persistence.
+     * @param vault - Encrypted vault object.
+     */
+    async insertVault(vault: IVault): Promise<void> {
+        await this.vaults.put(vault);
+    }
+
+    /**
+     * Retrieve an encrypted vault from IndexedDB by its ID.
+     * @param id - Vault ID used to fetch encrypted version.
+     * @returns {IVault | undefined}
+     */
+    async getVault(id: string): Promise<IVault | undefined> {
+        const vault = await this.vaults.get(id);
+        return Promise.resolve(vault);
+    }
+
+    /**
+     * Removes an encrypted vault from IndexedDB by its ID.
+     * @param id - Vault ID to be deleted.
+     * @returns {void}
+     */
+    async removeVault(id: string): Promise<void> {
+        try {
+            await this.vaults.delete(id);
+        } catch (e) {
+            return Promise.reject(e);
+        }
     }
 }
 
