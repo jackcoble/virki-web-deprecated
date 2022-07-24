099687359991ffe348ba110ac620ca531e7c42d0
Jack <jack@virki.io>
2022-07-24 22:29:07 +0100

    Register Vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
