b6078f98881419a5a8d757f807c59a7ed86da8b2
Jack <jack@virki.io>
2022-07-24 12:08:36 +0100

    Enforce 5 second timeout when making request to Status endpoint

--- src/service/api/system.ts
+++ src/service/api/system.ts
@@ -4,6 +4,8 @@ import { api } from "./api";
 export default {
     // Status/healthcheck endpoint
     Status(): Promise<AxiosResponse> {
-        return api.get("/v1/status");
+        return api.get("/v1/status", {
+            timeout: 5 * 1000 // 5 second tinmeout
+        });
     }
 }
