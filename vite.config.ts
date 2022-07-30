7635ccb3c10799a287cc355e01e2106ceabdede7
Jack <jack@virki.io>
2022-07-30 18:39:12 +0100

    Remove PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    vue()
   ],
   resolve: {
     alias: {
