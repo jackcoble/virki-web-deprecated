83cfb6f49db80922c36ae159acb08154f779f95f
Jack <jack@virki.io>
2022-08-01 19:49:28 +0100

    Populate PouchDB constructor information from Store

--- src/composables/usePouchDB.ts
+++ src/composables/usePouchDB.ts
@@ -2,12 +2,10 @@ import { Database } from "@/class/pouchdb";
 import { useApplicationStore } from "@/stores/appStore";
 import { useAuthenticationStore } from "@/stores/authenticationStore";
 
-const REMOTE_HOST = "http://localhost:5984"
-
 export default function usePouchDB () {
     // Fetch access token from Authentication store
     const authenticationStore = useAuthenticationStore();
     const applicationStore = useApplicationStore();
     
-    return new Database(authenticationStore.getAccessToken!, applicationStore.getSyncDB, REMOTE_HOST)
+    return new Database(parseInt(applicationStore.getSync.type), applicationStore.getSync.db, applicationStore.getSync.url, authenticationStore.getAccessToken!)
 }
