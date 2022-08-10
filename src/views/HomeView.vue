e2d3e05622eb897ff8603f55e42cf72e2aeded1c
Jack <jack@virki.io>
2022-08-10 14:40:21 +0100

    Remove search field from home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,23 +24,6 @@
         </div>
       </div>
 
-      <div class="flex-row">
-        <label for="default-search"
-          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-        <div class="relative">
-          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-            </svg>
-          </div>
-          <input type="search" id="default-search"
-            class="pl-10 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2"
-            placeholder="Search entries..." required>
-        </div>
-      </div>
-
       <div class="flex flex-col">
         <div v-if="entries && entries.length !== 0" v-for="entry in entries" :key="entry">
           <div class="w-full border-t border-gray-300"></div>
