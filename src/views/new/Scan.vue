9fb03abc6f4ac58e2a4d88c906bc95ac6f06a167
Jack <jack@virki.io>
2022-08-10 22:29:50 +0100

    Scanner and manual entry is now on a single page

--- src/views/new/Scan.vue
+++ src/views/new/Scan.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="h-screen w-full">
+    <div v-if="!hideScanner" class="flex flex-col items-center h-screen w-full">
+        <b-button class="absolute z-10 w-48 bottom-20" @click="hideScanner = true">Enter manually?</b-button>
         <qrcode-stream @decode="onDecode"></qrcode-stream>
+    </div>
+    
+    <div v-else>
+        <p>Manual token entry...</p>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
     name: "Scan",
     setup() {
+        const hideScanner = ref(false);
+
         const onDecode = (data: any) => {
-            console.log(data);
+            // TODO: Parse the OTP string URL in the data
+
+            // TODO: Populate fields
+
+            // Hide the scanner
+            hideScanner.value = true;
         }
 
         return {
+            hideScanner,
             onDecode
         }
     }
