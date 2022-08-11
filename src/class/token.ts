1a2247f330a03b0188d7b249457c2d456d205dfb
Jack <jack@virki.io>
2022-08-11 11:04:46 +0100

    Token method to create encrypted object

--- src/class/token.ts
+++ src/class/token.ts
@@ -1,4 +1,6 @@
 import * as CryptoJS from "crypto-js";
+import type { Token as TokenModel } from "@/models/token";
+import { Crypto } from "./crypto";
 
 export enum OTPType {
     TOTP = "totp",
@@ -13,84 +15,43 @@ export enum OTPAlgorithm {
 }
 
 export class Token {
-    private static dec2hex(s: number): string {
-        return (s < 15.5 ? "0" : "") + Math.round(s).toString(16);
-    }
+    async createEncryptedTokenObject(token: TokenModel, vaultSymmetricKey: string): Promise<TokenModel> {
+        // The token document _type should be "token"
+        token._type = "token";
 
-    private static hex2dec(s: string): number {
-        return Number(`0x${s}`);
-    }
+        // Create a new object that will contain our encrypted data
+        const encryptedToken = Object.assign({}, token);
 
-    private static hex2str(hex: string) {
-        let str = "";
-        for (let i = 0; i < hex.length; i += 2) {
-            str += String.fromCharCode(this.hex2dec(hex.substr(i, 2)));
-        }
-        return str;
-    }
-
-    private static leftpad(str: string, len: number, pad: string): string {
-        if (len + 1 >= str.length) {
-            str = new Array(len + 1 - str.length).join(pad) + str;
-        }
-        return str;
-    }
-
-    private static base32tohex(base32: string): string {
-        const base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-        let bits = "";
-        let hex = "";
-        let padding = 0;
-
-        for (let i = 0; i < base32.length; i++) {
-            if (base32.charAt(i) === "=") {
-                bits += "00000";
-                padding++;
-            } else {
-                const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
-                bits += this.leftpad(val.toString(2), 5, "0");
-            }
+        // Generate an ID for the token if it hasn't been generated already.
+        if (!token._id) {
+            let tokenId = window.crypto.randomUUID().replace(/-/g, "");
+            encryptedToken._id = `${token._type}:${tokenId}`;
         }
 
-        for (let i = 0; i + 4 <= bits.length; i += 4) {
-            const chunk = bits.substr(i, 4);
-            hex = hex + Number(`0b${chunk}`).toString(16);
+        // Using the vault symmetric key, we can encrypt the relevant data:
+        // - Issuer
+        // - Label
+        // - Secret
+        // - Icon (if present)
+        const symmetricEncryptionKey = await Crypto.fromBase64(vaultSymmetricKey);
+
+        encryptedToken.issuer = await Crypto.encrypt(await Crypto.fromString(token.issuer), symmetricEncryptionKey);
+        encryptedToken.label = await Crypto.encrypt(await Crypto.fromString(token.label), symmetricEncryptionKey);
+        encryptedToken.secret = await Crypto.encrypt(await Crypto.fromString(token.secret), symmetricEncryptionKey);
+        if (token.icon) {
+            encryptedToken.icon = await Crypto.encrypt(await Crypto.fromString(token.icon), symmetricEncryptionKey);
         }
 
-        switch (padding) {
-            case 0:
-                break;
-            case 6:
-                hex = hex.substr(0, hex.length - 8);
-                break;
-            case 4:
-                hex = hex.substr(0, hex.length - 6);
-                break;
-            case 3:
-                hex = hex.substr(0, hex.length - 4);
-                break;
-            case 1:
-                hex = hex.substr(0, hex.length - 2);
-                break;
-            default:
-                throw new Error("Invalid Base32 string");
+        // Update or set the modified timestamp as current device UNIX time (milliseconds)
+        const currentUnixMilliseconds = Math.floor(Date.now());
+        encryptedToken.modified = currentUnixMilliseconds;
+
+        // Set the created time as current UNIX time if not already set
+        if (!token.created) {
+            encryptedToken.created = currentUnixMilliseconds;
         }
 
-        return hex;
-    }
-
-    private static base26(num: number) {
-        const chars = "23456789BCDFGHJKMNPQRTVWXY";
-        let output = "";
-        const len = 5;
-        for (let i = 0; i < len; i++) {
-            output += chars[num % chars.length];
-            num = Math.floor(num / chars.length);
-        }
-        if (output.length < len) {
-            output = new Array(len - output.length + 1).join(chars[0]) + output;
-        }
-        return output;
+        return Promise.resolve(encryptedToken);
     }
 
     // Generate a TOTP
@@ -206,5 +167,88 @@ export class Token {
             otp = new Array(len - otp.length + 1).join("0") + otp;
         }
         return otp.substr(otp.length - len, len).toString();
+    }
+
+    // =================================
+    // Utilities to help generate tokens
+    // =================================
+    private static dec2hex(s: number): string {
+        return (s < 15.5 ? "0" : "") + Math.round(s).toString(16);
+    }
+
+    private static hex2dec(s: string): number {
+        return Number(`0x${s}`);
+    }
+
+    private static hex2str(hex: string) {
+        let str = "";
+        for (let i = 0; i < hex.length; i += 2) {
+            str += String.fromCharCode(this.hex2dec(hex.substr(i, 2)));
+        }
+        return str;
+    }
+
+    private static leftpad(str: string, len: number, pad: string): string {
+        if (len + 1 >= str.length) {
+            str = new Array(len + 1 - str.length).join(pad) + str;
+        }
+        return str;
+    }
+
+    private static base32tohex(base32: string): string {
+        const base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        let bits = "";
+        let hex = "";
+        let padding = 0;
+
+        for (let i = 0; i < base32.length; i++) {
+            if (base32.charAt(i) === "=") {
+                bits += "00000";
+                padding++;
+            } else {
+                const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+                bits += this.leftpad(val.toString(2), 5, "0");
+            }
+        }
+
+        for (let i = 0; i + 4 <= bits.length; i += 4) {
+            const chunk = bits.substr(i, 4);
+            hex = hex + Number(`0b${chunk}`).toString(16);
+        }
+
+        switch (padding) {
+            case 0:
+                break;
+            case 6:
+                hex = hex.substr(0, hex.length - 8);
+                break;
+            case 4:
+                hex = hex.substr(0, hex.length - 6);
+                break;
+            case 3:
+                hex = hex.substr(0, hex.length - 4);
+                break;
+            case 1:
+                hex = hex.substr(0, hex.length - 2);
+                break;
+            default:
+                throw new Error("Invalid Base32 string");
+        }
+
+        return hex;
+    }
+
+    private static base26(num: number) {
+        const chars = "23456789BCDFGHJKMNPQRTVWXY";
+        let output = "";
+        const len = 5;
+        for (let i = 0; i < len; i++) {
+            output += chars[num % chars.length];
+            num = Math.floor(num / chars.length);
+        }
+        if (output.length < len) {
+            output = new Array(len - output.length + 1).join(chars[0]) + output;
+        }
+        return output;
     }
 }
