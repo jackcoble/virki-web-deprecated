5d7239610f2faaf03df2b3ef05953c23bd3375e2
Jack <jack@virki.io>
2022-08-11 11:31:16 +0100

    Add vault and tags property to token model

--- src/models/token.ts
+++ src/models/token.ts
@@ -4,6 +4,7 @@ type Token = {
     _id: string;
     _rev?: string;
     type: string;
+    vault: string; // Vault ID the token belongs to
 
     issuer: string;
     label: string; // This is the username value
@@ -14,6 +15,8 @@ type Token = {
     otp_type: OTPType;
     period?: number;
     counter?: number;
+
+    tags: string[]; // IDs of tags
 
     modified:  number;
     created: number;
