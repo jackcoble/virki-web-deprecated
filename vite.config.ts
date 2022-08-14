beed49937e56c16fbb9dd31436d1eeabeb259585
Jack <jack@virki.io>
2022-08-14 18:04:41 +0100

    Enable PWA in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
