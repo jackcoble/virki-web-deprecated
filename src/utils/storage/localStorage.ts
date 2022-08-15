b346e5d14735de1a090fab12fae7385ea98660e8
Jack <jack@virki.io>
2022-08-15 18:39:21 +0100

    Storage utility for LocalStorage

---
+++ src/utils/storage/localStorage.ts
@@ -0,0 +1,43 @@
+export enum LS_KEYS {
+    AUTH_TOKENS = "authenticationTokens",
+    ENCRYPTED_MASTER_KEYPAIR = "encryptedMasterKeypair",
+    PASSWORD = "password",
+    USER_DETAILS = "userDetails"
+}
+
+/**
+ * Store an object in LocalStorage.
+ * @param key 
+ * @param value 
+ */
+export const setData = (key: LS_KEYS, value: object) => {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * Remove an object by key from LocalStorage.
+ * @param key 
+ */
+export const removeData = (key: LS_KEYS) => {
+    localStorage.removeItem(key);
+}
+
+/**
+ * Retrieves data from LocalStorage by key, and returns the parsed JSON object.
+ * @param key 
+ * @returns {object}
+ */
+export const getData = (key: LS_KEYS) => {
+    try {
+        // Check the key we're after exists in LocalStorage
+        if (localStorage.getItem(key) === undefined) {
+            return null;
+        }
+
+        // Attempt to parse the data
+        const data = localStorage.getItem(key);
+        return data && JSON.parse(data);
+    } catch (error) {
+        throw new Error("Unable to parse JSON for key: " + key);
+    }
+}
