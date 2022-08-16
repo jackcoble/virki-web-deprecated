4e1b76a23d3caf8ac2613229886f600a9a1c5d12
Jack <jack@virki.io>
2022-08-17 00:10:55 +0100

    Configure Vite for use with Comlink

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import { VitePWA } from 'vite-plugin-pwa'
+import { comlink } from 'vite-plugin-comlink'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,8 +15,14 @@ export default defineConfig({
       devOptions: {
         enabled: true
       }
-    })
+    }),
+    comlink()
   ],
+  worker: {
+    plugins: [
+      comlink()
+    ]
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
