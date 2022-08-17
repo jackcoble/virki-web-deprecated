004d43b74cea69a1174f0dc10a65d1047abf9c99
Jack <jack@virki.io>
2022-08-17 16:38:04 +0100

    Rename user API to userService

--- src/service/api/userService.ts
+++ src/service/api/userService.ts
@@ -2,6 +2,12 @@ import { api } from "@/service/api/api";
 import type { AxiosResponse } from "axios";
 
 export default {
+    SendOTP(email: string): Promise<AxiosResponse> {
+        return api.post("/v1/users/otp", {
+            email: email
+        })
+    },
+
     GetAccount(): Promise<AxiosResponse> {
         return api.get("/v1/account");
     }
