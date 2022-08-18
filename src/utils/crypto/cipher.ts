ccb6c670648f798b9dd409d5e4622a993956d6c8
Jack <jack@virki.io>
2022-08-18 21:20:53 +0100

    Cipher crypto utility to parse and serialise

---
+++ src/utils/crypto/cipher.ts
@@ -0,0 +1,68 @@
+import { EncryptionType, type Cipher } from "@/types/crypto";
+
+/**
+ * Parses a "cipher" string into a usable object.
+ * @param cipherString 
+ * @returns {object}
+ */
+export function parseCipherString(cipherString: string): Promise<Cipher> {
+    /*
+        A "cipher" string typically follows this format
+        EncryptionType.CipherText|Nonce|MAC
+
+        We want to parse a "cipher" string and return the contents
+        of it in an object.
+    */
+
+    // Split the "cipher" string by the period character, so we can validate the EncryptionType.
+    const encryptionType = cipherString.split(".")[0] as EncryptionType;
+    if (!Object.values(EncryptionType).includes(encryptionType)) {
+        return Promise.reject("Encryption type is invalid!");
+    }
+
+    // Depending on the EncryptionType, a "cipher" string may be formatted differently,
+    // possibly due to additional options being required for that encryption algorithm.
+    // So depending on the algorithm, we can parse it accordingly.
+    const cipherSplit = cipherString.split(".")[1].split("|");
+
+    switch (encryptionType) {
+        case EncryptionType.XCHACHA20_POLY1305:
+            const cipher: Cipher = {
+                type: EncryptionType.XCHACHA20_POLY1305,
+                ciphertext: cipherSplit[0],
+                nonce: cipherSplit[1],
+                mac: cipherSplit[2]
+            }
+
+            return Promise.resolve(cipher);
+    
+        default:
+            return Promise.reject("Unexpected error parsing cipher string!")
+    }
+}
+
+/**
+ * Serialise a "cipher" string to represent encrypted data.
+ * @param encryptionType
+ * @param cipherText 
+ * @param nonce 
+ * @param mac 
+ * @returns {string}
+ */
+export function serialiseCipherString(encryptionType: EncryptionType, cipherText: string, nonce: string, mac: string): Promise<string> {
+    // Validate the encryptionType against the enum.
+    if (!Object.values(EncryptionType).includes(encryptionType)) {
+        return Promise.reject("Encryption type is invalid!");
+    }
+
+    switch (encryptionType) {
+        case EncryptionType.XCHACHA20_POLY1305:
+            // XChaCha20-Poly1305 ciphers must be represented in the following format:
+            // xchacha20poly1305.CipherText|Nonce|MAC
+            const cipherString = `${EncryptionType.XCHACHA20_POLY1305}.${cipherText}|${nonce}|${mac}`;
+            return Promise.resolve(cipherString);
+    
+        default:
+            return Promise.reject("Unexpected error serialising cipher string!");
+    }
+}
