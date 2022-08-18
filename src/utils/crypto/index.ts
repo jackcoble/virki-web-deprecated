854ec4972fee53825bbb762560898b167d0238e1
Jack <jack@virki.io>
2022-08-18 22:16:00 +0100

    generateKeys should return cipher strings

--- src/utils/crypto/index.ts
+++ src/utils/crypto/index.ts
@@ -1,5 +1,7 @@
+import { EncryptionType } from "@/types/crypto";
 import type { Keys } from "@/types/user";
 import * as libsodium from "@/utils/crypto/libsodium";
+import { serialiseCipherString } from "./cipher";
 
 export async function generateKeys(password: string) {
     // Master and recovery keys
@@ -18,9 +20,15 @@ export async function generateKeys(password: string) {
     const masterKeyEncryptedWithRecoveryKey = await libsodium.encryptToB64(masterKey, recoveryKey);
     const recoveryKeyEncryptedWithMasterKey = await libsodium.encryptToB64(recoveryKey, masterKey);
 
+    // Generate cipher strings for all of the keys
+    const masterKeyEncryptedWithKekCipherString = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, masterKeyEncryptedWithKek.ciphertext, masterKeyEncryptedWithKek.nonce, masterKeyEncryptedWithKek.mac);
+    const masterKeyEncryptedWithRecoveryKeyCipherString = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, masterKeyEncryptedWithRecoveryKey.ciphertext, masterKeyEncryptedWithRecoveryKey.nonce, masterKeyEncryptedWithRecoveryKey.mac);
+    const recoveryKeyEncryptedWithMasterKeyCipherString = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, recoveryKeyEncryptedWithMasterKey.ciphertext, recoveryKeyEncryptedWithMasterKey.nonce, recoveryKeyEncryptedWithMasterKey.mac);
+
     // Generate an X25519 keypair and encrypt private key with master key.
     const keypair = await libsodium.generateKeypair();
     const encryptedKeypair = await libsodium.encryptToB64(keypair.privateKey, masterKey);
+    const encryptedKeyPairCipherString = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, encryptedKeypair.ciphertext, encryptedKeypair.nonce, encryptedKeypair.mac);
 
     // Construct a Keys object containing everything we need for encryption.
     const keys: Keys = {
@@ -29,21 +37,14 @@ export async function generateKeys(password: string) {
             opsLimit: kek.opsLimit,
             memLimit: kek.memLimit
         },
-        master: {
-            key: masterKeyEncryptedWithKek.ciphertext,
-            nonce: masterKeyEncryptedWithKek.nonce
-        },
+        master_encryption_key: masterKeyEncryptedWithKekCipherString,
         keypair: {
             publicKey: keypair.publicKey,
-            privateKey: encryptedKeypair.ciphertext,
-            privateKeyNonce: encryptedKeypair.nonce
+            privateKey: encryptedKeyPairCipherString
         },
         recovery: {
-            masterKey: masterKeyEncryptedWithRecoveryKey.ciphertext,
-            masterKeyNonce: masterKeyEncryptedWithRecoveryKey.nonce,
-
-            recoveryKey: recoveryKeyEncryptedWithMasterKey.ciphertext,
-            recoveryKeyNonce: recoveryKeyEncryptedWithMasterKey.nonce
+            masterKeyEncryptedWithRecoveryKey: masterKeyEncryptedWithRecoveryKeyCipherString,
+            recoveryKeyEncryptedWithMasterKey: recoveryKeyEncryptedWithMasterKeyCipherString,
         }
     }
     
