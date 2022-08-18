34f014936436090067226649fb244eea6b3b5839
Jack <jack@virki.io>
2022-08-18 22:33:26 +0100

    Crypto utility to decrypt keys

--- src/utils/crypto/index.ts
+++ src/utils/crypto/index.ts
@@ -1,9 +1,9 @@
 import { EncryptionType } from "@/types/crypto";
 import type { Keys } from "@/types/user";
 import * as libsodium from "@/utils/crypto/libsodium";
-import { serialiseCipherString } from "./cipher";
+import { serialiseCipherString, parseCipherString } from "./cipher";
 
-export async function generateKeys(password: string) {
+export async function generateKeys(password: string): Promise<Keys> {
     // Master and recovery keys
     const masterKey = await libsodium.generateEncryptionKey();
     const recoveryKey = await libsodium.generateEncryptionKey();
@@ -49,4 +49,69 @@ export async function generateKeys(password: string) {
     }
     
     return Promise.resolve(keys);
+}
+
+export async function decryptKeys(passphrase: string, encryptedKeys: Keys): Promise<Keys> {
+    // Derive Key Encryption Key
+    const kek = await libsodium.deriveKeyEncryptionKey(passphrase, encryptedKeys.kek.salt, encryptedKeys.kek.opsLimit, encryptedKeys.kek.memLimit);
+    if (!kek) {
+        return Promise.reject("Key encryption key cannot be null!");
+    }
+
+    // Decrypt the master encryption key into Base64
+    const mekCipher = await parseCipherString(encryptedKeys.master_encryption_key);
+    const masterEncryptionKey = await libsodium.decryptFromB64(
+        mekCipher.ciphertext,
+        mekCipher.mac,
+        mekCipher.nonce,
+        kek.key
+    );
+
+    // Decrypt the keypair with the master encryption key
+    const kpCipher = await parseCipherString(encryptedKeys.keypair.privateKey);
+    const kpPrivateKey = await libsodium.decryptFromB64(
+        kpCipher.ciphertext,
+        kpCipher.mac,
+        kpCipher.nonce,
+        masterEncryptionKey
+    )
+
+    // Decrypt the recovery keys
+    // Master key encrypted with recovery key
+    const mkewrkCipher = await parseCipherString(encryptedKeys.recovery.masterKeyEncryptedWithRecoveryKey);
+    const mkewrk = await libsodium.decryptFromB64(
+        mkewrkCipher.ciphertext,
+        mkewrkCipher.mac,
+        mkewrkCipher.nonce,
+        masterEncryptionKey
+    )
+
+    // Recovery key encrypted with master key
+    const rkewmkCipher = await parseCipherString(encryptedKeys.recovery.recoveryKeyEncryptedWithMasterKey);
+    const rkewmk = await libsodium.decryptFromB64(
+        rkewmkCipher.ciphertext,
+        rkewmkCipher.mac,
+        rkewmkCipher.nonce,
+        masterEncryptionKey
+    )
+
+    // Construct an object to be returned containing the decrypted keys
+    const decryptedKeys: Keys = {
+        kek: {
+            salt: kek.salt,
+            opsLimit: kek.opsLimit,
+            memLimit: kek.memLimit
+        },
+        master_encryption_key: masterEncryptionKey,
+        keypair: {
+            publicKey: encryptedKeys.keypair.publicKey,
+            privateKey: kpPrivateKey
+        },
+        recovery: {
+            masterKeyEncryptedWithRecoveryKey: mkewrk,
+            recoveryKeyEncryptedWithMasterKey: rkewmk,
+        }
+    }
+
+    return Promise.resolve(decryptedKeys);
 }
--- src/worker/crypto.worker.ts
+++ src/worker/crypto.worker.ts
@@ -1,10 +1,15 @@
 import { expose } from "comlink";
 import * as crypto from "@/utils/crypto";
+import type { Keys } from "@/types/user";
 
 export const Crypto = {
     generateKeys (passphrase: string) {
         return crypto.generateKeys(passphrase);
-    } 
+    },
+
+    decryptKeys (passphrase: string, keys: Keys) {
+        return crypto.decryptKeys(passphrase, keys);
+    }
 }
 
 expose(Crypto)
