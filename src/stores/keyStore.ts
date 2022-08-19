1aa1790a14e1ce08ed0461c56a223ae885aefb0b
Jack <jack@virki.io>
2022-08-19 02:00:25 +0100

    Store session token in keyStore

--- src/stores/keyStore.ts
+++ src/stores/keyStore.ts
@@ -4,14 +4,20 @@ import { defineStore } from 'pinia'
 export const useKeyStore = defineStore({
   id: 'keyStore',
   state: () => ({
+    sessionToken: "",
     masterEncryptionKey: getKey(SESSION_KEYS.MASTER_ENCRYPTION_KEY).key || ""
   }),
 
   getters: {
+    getSessionToken: (state) => state.sessionToken,
     getMasterEncryptionKey: (state) => state.masterEncryptionKey
   },
 
   actions: {
+    setSessionToken(token: string) {
+        this.sessionToken = token;
+    },
+
     setMasterEncryptionKey(key: string) {
         this.masterEncryptionKey = key;
 
