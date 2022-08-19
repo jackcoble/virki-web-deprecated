8d9efe1d5b6bca42c9731346d7e542f6fa78b2d9
Jack <jack@virki.io>
2022-08-19 01:16:37 +0100

    Create Pinia store for keys

---
+++ src/stores/keyStore.ts
@@ -0,0 +1,22 @@
+import { SESSION_KEYS, setKey } from '@/utils/storage/sessionStorage';
+import { defineStore } from 'pinia'
+
+export const useKeyStore = defineStore({
+  id: 'keyStore',
+  state: () => ({
+    masterEncryptionKey: ""
+  }),
+
+  getters: {
+    getMasterEncryptionKey: (state) => state.masterEncryptionKey
+  },
+
+  actions: {
+    setMasterEncryptionKey(key: string) {
+        this.masterEncryptionKey = key;
+
+        // Persist key in session storage
+        setKey(SESSION_KEYS.MASTER_ENCRYPTION_KEY, key);
+    }
+  },
+})
