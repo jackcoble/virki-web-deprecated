602228609272229a0f3cb45d343c598107e94cc0
Jack <jack@virki.io>
2022-08-20 23:10:44 +0100

    Fix frustrating bug with encryption keys being returned directly from the store

--- src/stores/keyStore.ts
+++ src/stores/keyStore.ts
@@ -13,7 +13,15 @@ export const useKeyStore = defineStore({
   getters: {
     getSessionToken: (state) => state.sessionToken,
     getMasterEncryptionKey: (state) => state.masterEncryptionKey,
-    getEncryptedKeys: (state) => state.encryptedKeys
+    getEncryptedKeys: (state) => {
+        // So after many hours of tearing my hair out, it turns out WebWorkers hate objects with function signatures being
+        // provided to them! So before returning the encrypted keys, stringify the object into JSON and then parse it again,
+        // just so we have a plain old JSON object.
+        const encryptedKeysStringified = JSON.stringify(state.encryptedKeys);
+        const parsedEncryptedKeys = JSON.parse(encryptedKeysStringified) as Keys;
+
+        return parsedEncryptedKeys;
+    }
   },
 
   actions: {
