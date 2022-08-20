18c64e21b18bf2f1b8ead46675b0cf8611b13e58
Jack <jack@virki.io>
2022-08-20 22:31:58 +0100

    Modify Comlink utility to export a CryptoWorker

--- src/utils/comlink/index.ts
+++ src/utils/comlink/index.ts
@@ -1,10 +1,18 @@
 import * as Comlink from "comlink";
 
+export interface ComlinkWorker {
+    comlink: any;
+    worker: Worker;
+}
+
+
 // Returns a Crypto worker
-export const getDedicatedCryptoWorker = (): any => {
+export const getDedicatedCryptoWorker = (): ComlinkWorker => {
     const workerFile = new URL("../../worker/crypto.worker.ts", import.meta.url);
     const worker = new Worker(workerFile, { type: "module" });
 
     const comlink = Comlink.wrap(worker);
-    return comlink;
+    return { comlink, worker };
 }
+
+export const CryptoWorker: any = getDedicatedCryptoWorker()?.comlink;
