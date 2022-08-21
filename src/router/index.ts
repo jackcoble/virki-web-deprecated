3890ff30fdf058f93f870c9cd5c9a6806423273a
Jack <jack@virki.io>
2022-08-21 14:55:48 +0100

    Router guard to check for session token and encrypted keys on certain routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import Verify from "@/views/Verify.vue"
 import Credentials from "@/views/Credentials.vue"
 import Vault from "@/views/Vault.vue"
 import { PAGES } from './pages'
+import { useKeyStore } from '@/stores/keyStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,22 +14,34 @@ const router = createRouter({
     {
       path: PAGES.ROOT,
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        public: true
+      }
     },
     {
       path: PAGES.REGISTER,
       name: "register",
-      component: Register
+      component: Register,
+      meta: {
+        public: true
+      }
     },
     {
       path: PAGES.VERIFY,
       name: 'verify',
-      component: Verify
+      component: Verify,
+      meta: {
+        public: true
+      }
     },
     {
       path: PAGES.CREDENTIALS,
       name: "credentials",
-      component: Credentials
+      component: Credentials,
+      meta: {
+        public: true
+      }
     },
     {
       path: PAGES.VAULT,
@@ -36,6 +49,30 @@ const router = createRouter({
       component: Vault
     }
   ]
+})
+
+/*
+We should assume that all pages by default require some form of authentication
+unless stated otherwise with the "public" property in the route metadata. To verify a user is authenticated, we'll just check for the
+presence of a session token in the store.
+*/
+router.beforeEach((to, from, next) => {
+  const keyStore = useKeyStore();
+  const session = keyStore.getSessionToken;
+
+  // If no session token is present and the page isn't public, redirect to root (login)
+  if (!session && !to.meta.public) {
+    return next({ path: PAGES.ROOT });
+  }
+
+  // So we have a session token present, but if we don't have encrypted keys on the device,
+  // we might not want to allow the user to visit a certain page, such as the "credentials" for decryption.
+  const encryptedKeys = keyStore.getEncryptedKeys;
+  if (!encryptedKeys.master_encryption_key && to.path === PAGES.CREDENTIALS) {
+    return next({ path: PAGES.ROOT });
+  }
+
+  next();
 })
 
 export default router
