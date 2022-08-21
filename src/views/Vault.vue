6935278b1550eaefc63796d962bf7eecf8b8463b
Jack <jack@virki.io>
2022-08-21 21:04:09 +0100

    If a user has no vaults available, encourage them to create one

--- src/views/Vault.vue
+++ src/views/Vault.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <div class="flex w-full justify-between items-center px-4 md:px-11 py-4 border-b-2 border-b-mountain-meadow bg-gray-100 space-x-3 shadow">
       <!-- Logo -->
-      <a href="/">
+      <a href="/vault">
         <img class="w-24 hidden md:block" src="@/assets/images/virki_full_horizontal_transparent_dark.png" alt="Virki Logo" />
       </a>
 
@@ -16,12 +16,19 @@
       <!-- Search input -->
       <b-input class="w-full md:w-3/6" type="search" placeholder="Search for an entry or tag..."></b-input>
 
-      <!-- New Entry button -->
+      <!-- New Entry or Vault button -->
       <div>
-        <b-button type="submit" classType="primary" @click="router.push('/new/qrcode')">
+        <b-button v-if="hasVaultsAvailable" type="submit" classType="primary">
           <div class="flex flex-row justify-center items-center space-x-1">
             <PlusCircleIcon class="w-5 md:-ml-1" />
             <span class="hidden md:block">New Entry</span>
+          </div>
+        </b-button>
+
+        <b-button v-if="!hasVaultsAvailable" type="submit" classType="primary">
+          <div class="flex flex-row justify-center items-center space-x-1">
+            <PlusCircleIcon class="w-5 md:-ml-1" />
+            <span class="hidden md:block">Create Vault</span>
           </div>
         </b-button>
       </div>
@@ -35,28 +42,37 @@
 
       <!-- Token entries -->
       <div class="flex-col flex-grow overflow-auto" >
-        <!-- Show frowny face if we've got no tokens -->
-        <div class="flex flex-col justify-center items-center h-full p-4 text-center space-y-2">
+        <!-- Show message if no vaults are available -->
+        <div v-if="!hasVaultsAvailable" class="flex flex-col justify-center items-center h-full p-4 text-center space-y-2">
+          <EmojiSadIcon class="w-24 text-mountain-meadow" />
+          <p class="text-sm">To get started, you need to create a Vault.</p>
+          <b-button class="w-36">
+            <div class="flex flex-row justify-center items-center space-x-1">
+              <PlusCircleIcon class="w-5 md:-ml-1" />
+              <span class="hidden md:block">Create Vault</span>
+            </div>
+          </b-button>
+        </div>
+
+        <!-- Show frowny face if we've got no tokens, but have vaults available -->
+        <div v-if="hasVaultsAvailable" class="flex flex-col justify-center items-center h-full p-4 text-center space-y-2">
           <EmojiSadIcon class="w-24 text-mountain-meadow" />
           <p class="text-sm">You have no authentication tokens in your vault.</p>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Create vault modal -->
-  <CreateVaultModal v-if="showCreateVaultModal" @close="showCreateVaultModal = !showCreateVaultModal" />
 </template>
 
 <script lang="ts">
 import useEmitter from "@/composables/useEmitter";
 
-import { computed, defineComponent, onBeforeUnmount, onMounted, ref } from "vue";
+import { defineComponent, ref } from "vue";
 
 import { EmojiSadIcon, PlusCircleIcon, ClockIcon, XIcon, MenuIcon } from "@heroicons/vue/outline"
 import { useRouter } from "vue-router";
 
-import Sidebar from "@/components/Sidebar.vue"
+import Sidebar from "@/components/Sidebar.vue";
 
 export default defineComponent({
   name: "HomeView",
@@ -73,6 +89,11 @@ export default defineComponent({
 
     const emitter = useEmitter();
 
+    // We don't want to allow any token related actions until the user
+    // has vaults available. If they have just signed up, then this value
+    // should be false.
+    const hasVaultsAvailable = ref(false);
+
     // Sidebar refs
     const closeMenuMobile = ref(false);
     const showSidebarVaults = ref(false);
@@ -82,7 +103,7 @@ export default defineComponent({
     return {
       router,
 
-    
+      hasVaultsAvailable,
 
       closeMenuMobile,
       showSidebarVaults,
