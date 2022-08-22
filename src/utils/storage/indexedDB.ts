76f4e7e0f8f91241539bc803bd247706028aa2d1
Jack <jack@virki.io>
2022-08-22 18:56:27 +0100

    IndexedDB storage utility for vaults

--- src/utils/storage/indexedDB.ts
+++ src/utils/storage/indexedDB.ts
@@ -1,27 +1,34 @@
-import * as localForage from "localforage";
+import type { Vault } from "@/types/vault";
+import { Dexie, type Table } from "dexie";
 
 /*
 As Virki is an offline first application, we need a reliable database we can use in the browser.
-For this, we can make use of IndexedDB - which we use LocalForage to help us with!
+For this, we can make use of IndexedDB - which we use Dexie to help us with!
 */
 const DB_NAME = "virki";
+const DB_VERSION = 1;
+
+class IndexedDB extends Dexie {
+    // 'vaults' is added by Dexie when declaring the stores()
+    // We just tell the typing system this is the case
+    vaults!: Table<Vault>
+
+    constructor() {
+        super(DB_NAME);
+        this.version(DB_VERSION).stores({
+            vaults: 'id'
+        })
+    }
+}
 
 // getDB should return an existing database for us, or create one if it doesn't exist.
-export const getDB = async (): Promise<LocalForage> => {
-    const db = localForage.createInstance({
-        name: DB_NAME,
-        version: 1.0,
-        driver: localForage.INDEXEDDB
-    });
-
-    return db;
+export const getDB = (): IndexedDB => {
+    return new IndexedDB();
 }
 
 // insertVault should add a vault into the database, or update an existing one if it happens
 // to have the same ID.
-export const insertVault = async (vault: any) => {
-    const db = await getDB();
-    
-    // Put into the DB
-    await db.setItem(vault.id, vault);
+export const insertVault = async (vault: Vault) => {
+    const db = getDB();
+    await db.vaults.put(vault);
 }
