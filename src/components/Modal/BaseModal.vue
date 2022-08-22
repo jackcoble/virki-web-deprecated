5692debc98a921ba82c143a41ef9e9932ae4d565
Jack <jack@virki.io>
2022-08-22 21:30:02 +0100

    BaseModal should be customisable via slots

--- src/components/Modal/BaseModal.vue
+++ src/components/Modal/BaseModal.vue
@@ -1,80 +1,41 @@
 <template>
   <teleport to="body">
-    <Transition
-      appear
-      enter-active-class="transition ease-out duration-200 transform"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="transition ease-in duration-200 transform"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <div
-        class="fixed z-10 inset-0 overflow-y-auto"
-        aria-labelledby="modal-title"
-        role="dialog"
-        aria-modal="true"
-      >
-        <div
-          class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-        >
+    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
           <div
-            @click="closeModal"
-            class="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"
-            aria-hidden="true"
-          ></div>
+            class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+            <div>
+              <!-- Icon -->
+              <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                <slot name="icon">
+                  <ExclamationCircleIcon class="h-6 w-6 text-red-600" />
+                </slot>
+              </div>
 
-          <!-- This element is to trick the browser into centering the modal contents. -->
-          <span
-            class="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-            >&#8203;</span
-          >
-
-          <div
-            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
-          >
-            <div
-              v-if="!hideCloseIcon"
-              class="hidden sm:block absolute top-0 right-0 pt-4 pr-4"
-            >
-              <button
-                @click="closeModal"
-                type="button"
-                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
-              >
-                <span class="sr-only">Close</span>
-                <XIcon class="w-6 h-6" />
-              </button>
-            </div>
-            <div class="sm:flex sm:items-start">
-              <div class="mt-3 sm:mt-0 sm:text-left w-full">
-                <slot name="body" />
+              <!-- Body -->
+              <div class="mt-3 text-center sm:mt-5">
+                <slot name="body"></slot>
               </div>
             </div>
 
-            <!-- Footer (make this dynamic for the different footers) -->
-            <div v-if="noFooter">
-              <slot name="footer" />
-            </div>
-            <div v-else>
-              <OK v-show="okFooter" @ok="$emit('ok')" @cancel="closeModal" />
-              <Done v-show="doneFooter" @done="$emit('done')" />
+            <!-- Footer -->
+            <div class="mt-5 sm:mt-6">
+              <slot name="footer">
+                <b-button classType="primary">OK</b-button>
+              </slot>
             </div>
           </div>
         </div>
       </div>
-    </Transition>
+    </div>
   </teleport>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, watch } from "vue";
-import { XIcon } from "@heroicons/vue/outline";
-
-// Footer components
-import OK from "@/components/Modal/Footers/OK.vue";
-import Done from "@/components/Modal/Footers/Done.vue";
+import { XIcon, ExclamationCircleIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
   name: "ModalDialog",
@@ -99,10 +60,8 @@ export default defineComponent({
     }
   },
   components: {
-    OK,
-    Done,
-
     XIcon,
+    ExclamationCircleIcon
   },
   setup(props, { emit }) {
     const closeModal = () => {
