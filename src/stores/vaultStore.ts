a693ce5627b91513b13a4333cb4ee38620d25729
Jack <jack@virki.io>
2022-08-23 17:24:09 +0100

    Store active vault

--- src/stores/vaultStore.ts
+++ src/stores/vaultStore.ts
@@ -1,4 +1,5 @@
 import type { Vault } from '@/types/vault';
+import { getData, LS_KEYS, setData } from '@/utils/storage/localStorage';
 import { defineStore } from 'pinia'
 
 /*
@@ -8,6 +9,7 @@ The vault store is dedicated to hold a decrypted copy of the vault data.
 export const useVaultStore = defineStore({
   id: 'vaultStore',
   state: () => ({
+    active: getData(LS_KEYS.ACTIVE_VAULT) || "",
     vaults: new Map<string, Vault>
   }),
 
@@ -19,12 +21,29 @@ export const useVaultStore = defineStore({
         })
 
         return vaults;
+    },
+
+    getActiveID: (state) => state.active,
+    getActive: (state) => {
+      const vault = state.vaults.get(state.active);
+      if (!vault) {
+        return;
+      }
+
+      return vault;
     }
   },
 
   actions: {
     add(vault: Vault) {
         this.vaults.set(vault.id, vault);
+    },
+
+    // Stores the ID of the last active vault
+    setActive(id: string) {
+      this.active = id;
+
+      setData(LS_KEYS.ACTIVE_VAULT, id);
     },
 
     // Clear all decrypted data from the Hash Map
