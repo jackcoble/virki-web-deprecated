f69e519b3ea4b2270d6ab8e1a8868abb88455694
Jack <jack@virki.io>
2022-08-25 15:21:50 +0100

    Remove avatar and username from sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,56 +1,6 @@
 <template>
     <!-- Sidebar -->
     <div class="flex flex-col h-full border-r-2 p-6 bg-gray-100 overflow-auto">
-
-        <!-- Avatar and user name -->
-        <div class="p-4">
-            <div class="flex items-center text-gray-800 cursor-pointer"
-                @click="showSidebarUserOptions = !showSidebarUserOptions">
-                <div class="flex flex-1 justify-start space-x-2">
-                    <UserIcon class="w-6" />
-                    <h2>{{ email }}</h2>
-                </div>
-
-                <div class="ml-2">
-                    <ChevronDownIcon class="w-4" />
-                </div>
-            </div>
-
-            <!-- User options -->
-            <div v-if="showSidebarUserOptions"
-                class="flex-col rounded shadow mt-3 text-gray-600 bg-white space-y-0.5 text-sm">
-                <!-- Recovery Key -->
-                <div class="flex items-center space-x-2 px-3 py-2 cursor-pointer rounded">
-                    <ShieldCheckIcon class="w-4" />
-                    <p>Recovery Key</p>
-                </div>
-
-                <!-- Change Password -->
-                <div class="flex items-center space-x-2 px-3 py-2 cursor-pointer rounded">
-                    <KeyIcon class="w-4" />
-                    <p>Change Password</p>
-                </div>
-
-                <!-- Change Email -->
-                <div class="flex items-center space-x-2 px-3 py-2 cursor-pointer rounded">
-                    <MailIcon class="w-4" />
-                    <p>Change Email Address</p>
-                </div>
-
-                <!-- Sessions management -->
-                <div class="flex items-center space-x-2 px-3 py-2 cursor-pointer rounded" @click="router.push(PAGES.SESSIONS)">
-                    <DeviceMobileIcon class="w-4" />
-                    <p>Active Sessions</p>
-                </div>
-
-                <!-- Logout -->
-                <div class="flex items-center space-x-2 px-3 py-2 cursor-pointer text-red-400 rounded" @click="doLogout">
-                    <LogoutIcon class="w-4" />
-                    <p>Logout</p>
-                </div>
-            </div>
-        </div>
-
         <!-- All Items and Favourites -->
         <div class="flex-col space-y-2 p-4 text-gray-700">
             <div class="flex py-2 items-center space-x-2">
