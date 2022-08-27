7ef0cc02b480de685e02a1d0cc13e2ec70722727
Jack <jack@virki.io>
2022-08-27 23:33:27 +0100

    Update the "new token" view

--- src/views/vaults/tokens/New.vue
+++ src/views/vaults/tokens/New.vue
@@ -1,113 +1,151 @@
 <template>
-    <div class="p-9 lg:w-4/5 xl:w-2/5 mx-auto space-y-2">
-        <h1 class="text-2xl text-center">Create a new token entry</h1>
-        <p>In order to store authentication tokens, you need a Vault. This allows you to organise your tokens and keep them in one place!</p>
+    <!-- Manual Entry -->
+    <div class="flex-col p-4 space-y-3 w-3/5 mx-auto">
+        <!-- Icon uploader -->
+        <div class="flex justify-center">
+            <b-icon-upload :image="token.icon" />
+        </div>
 
-        <form @submit.prevent="handleCreateVault" class="space-y-4 pt-4 pb-2 mx-auto">
-            <b-icon-upload class="mx-auto" @imageData="vault.icon = $event"></b-icon-upload>
-            <b-input placeholder="Vault name" v-model="vault.name" required></b-input>
-            <b-text-area placeholder="Description of this Vault" v-model="vault.description"></b-text-area>
-            
-            <div class="flex space-x-2">
-                <b-button type="submit" :loading="isCreatingVault">Create</b-button>
-                <b-button classType="danger" @click="router.push(PAGES.ROOT)">Cancel</b-button>
+        <!-- Title/Issuer -->
+        <div>
+            <label for="issuer" class="block mb-2 font-medium text-gray-900">Title (Issuer)</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <GlobeIcon class="w-5 h-5 text-gray-500" />
+                </div>
+                <input type="text" id="issuer"
+                    v-model="token.issuer"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                    placeholder="Twitter">
             </div>
-        </form>
+        </div>
+
+        <!-- Label -->
+        <div>
+            <label for="username" class="block mb-2 font-medium text-gray-900">Label</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <UserIcon class="w-5 h-5 text-gray-500" />
+                </div>
+                <input type="text" id="label"
+                    v-model="token.label"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                    placeholder="user@example.tld">
+            </div>
+        </div>
+
+        <!-- Secret/Seed -->
+        <div>
+            <label for="secret" class="block mb-2 font-medium text-gray-900">Secret/Seed</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <KeyIcon class="w-5 h-5 text-gray-500" />
+                </div>
+                <input type="text" id="secret"
+                    v-model="token.secret"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                    placeholder="JBSWY3DPEHPK3PXP">
+            </div>
+        </div>
+
+        <!-- Algorithm -->
+        <div>
+            <label for="algorithm" class="block mb-2 font-medium text-gray-900">Algorithm</label>
+            <select id="algorithm"
+                v-model="token.algorithm"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option :value="OTPAlgorithm.SHA1">SHA-1</option>
+                <option :value="OTPAlgorithm.SHA256">SHA-256</option>
+                <option :value="OTPAlgorithm.SHA512">SHA-512</option>
+            </select>
+        </div>
+
+        <!-- Token Length -->
+        <div>
+            <label for="length" class="block mb-2 font-medium text-gray-900">Token Length</label>
+            <select id="length"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option :value="6">6 digits</option>
+                <option :value="7">7 digits</option>
+                <option :value="8">8 digits</option>
+            </select>
+        </div>
+
+        <!-- Token Type -->
+        <div>
+            <label for="type" class="block mb-2 font-medium text-gray-900">Type</label>
+            <select id="type"
+                v-model="token.type"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <option :value="OTPType.TOTP">Time Based (TOTP)</option>
+                <option :value="OTPType.HOTP">Counter Based (HOTP)</option>
+                <option :value="OTPType.Steam">Steam</option>
+            </select>
+        </div>
+
+        <!-- Depending on the Token Type, some more information may be needed -->
+        <!-- Time Period (TOTP) -->
+        <div v-if="token.type === OTPType.TOTP">
+            <label for="period" class="block mb-2 font-medium text-gray-900">Time Period</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <ClockIcon class="w-5 h-5 text-gray-500" />
+                </div>
+                <input type="number" id="period" min="0"
+                    v-model.number="token.period"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5">
+            </div>
+        </div>
+
+        <!-- Initial Counter (HOTP) -->
+        <div v-if="token.type === OTPType.HOTP">
+            <label for="counter" class="block mb-2 font-medium text-gray-900">Initial Counter</label>
+            <div class="relative">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <ClockIcon class="w-5 h-5 text-gray-500" />
+                </div>
+                <input type="number" id="counter" min="0"
+                    v-model.number="token.counter"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5">
+            </div>
+        </div>
+
+        <!-- Save token button -->
+        <b-button @click="handleCreateToken">Add Token</b-button>
     </div>
 </template>
 
 <script lang="ts">
-import useToaster from '@/composables/useToaster';
 import { PAGES } from '@/router/pages';
-import { useKeyStore } from '@/stores/keyStore';
-import { useVaultStore } from '@/stores/vaultStore';
-import { EncryptionType } from '@/types/crypto';
-import type { Vault } from '@/types/vault';
-import { CryptoWorker } from '@/utils/comlink';
-import { sleep } from '@/utils/common';
-import { serialiseCipherString } from '@/utils/crypto/cipher';
-import { insertVault } from '@/utils/storage/indexedDB';
-import { defineComponent, onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { OTPType, OTPAlgorithm, type Token } from "@/types/token";
+import { GlobeIcon, UserIcon, KeyIcon, ClockIcon, PencilIcon, QrcodeIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
     name: "NewToken",
+    components: {
+        GlobeIcon,
+        UserIcon,
+        KeyIcon,
+        ClockIcon,
+        PencilIcon,
+        QrcodeIcon,
+    },
     setup() {
         const router = useRouter();
-        const route = useRoute();
 
-        const keyStore = useKeyStore();
-        const vaultStore = useVaultStore();
-        const toaster = useToaster();
+        const token = ref({
+            // Set some defaults...
+            algorithm: OTPAlgorithm.SHA1,
+            type: OTPType.TOTP,
+            period: 30,
+        } as Token);
 
-        const vault = ref({} as Vault);
-        const isCreatingVault = ref(false);
-
-        // Handle creating an encrypted vault using the data from the "vault" ref.
-        const handleCreateVault = async () => {
-            isCreatingVault.value = true;
-
-            // Derive an encryption key for the vault. This is what all entries under this vault will use to encrypt data.
-            const cryptoWorker = await new CryptoWorker();
-            const vaultEncryptionKey = await cryptoWorker.generateEncryptionKey();
-
-            // We want to encrypt the vault encryption key with our master encryption key.
-            const masterEncryptionKey = keyStore.getMasterEncryptionKey;
-            if (!masterEncryptionKey) {
-                return toaster.error("Master encryption key not present on device!");
-            }
-
-            // Serialise this into a cipher string
-            const encryptedVaultKeyObject = await cryptoWorker.encryptToB64(vaultEncryptionKey, masterEncryptionKey);
-            const encryptedVaultKey = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, encryptedVaultKeyObject.ciphertext, encryptedVaultKeyObject.nonce, encryptedVaultKeyObject.mac);
-
-            // We need to then encrypt the name, description and icon with the vault encryption key.
-            let encryptedName, encryptedDescription, encryptedIcon;
-            const encryptedNameObject = await cryptoWorker.encryptUTF8(vault.value.name, vaultEncryptionKey);
-            encryptedName = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, encryptedNameObject.ciphertext, encryptedNameObject.nonce, encryptedNameObject.mac);
-            
-            if (vault.value.description) {
-                const encryptedDescriptionObject = await cryptoWorker.encryptUTF8(vault.value.description, vaultEncryptionKey);
-                encryptedDescription = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, encryptedDescriptionObject.ciphertext, encryptedDescriptionObject.nonce, encryptedDescriptionObject.mac);
-            }
-            if (vault.value.icon) {
-                const encryptedIconObject = await cryptoWorker.encryptUTF8(vault.value.icon, vaultEncryptionKey);
-                encryptedIcon = await serialiseCipherString(EncryptionType.XCHACHA20_POLY1305, encryptedIconObject.ciphertext, encryptedIconObject.nonce, encryptedIconObject.mac);
-            }
-
-            // Construct a vault object with the encrypted data
-            const vaultId = window.crypto.randomUUID();
-            const createdDate = Math.floor(new Date().getTime() * 1000); // Unix microseconds
-
-            const encryptedVaultObject: Vault = {
-                id: vaultId,
-                key: encryptedVaultKey,
-                name: encryptedName,
-                description: encryptedDescription,
-                icon: encryptedIcon,
-                created: createdDate,
-                modified: createdDate
-            }
-
-            // Store the encrypted vault in IndexedDB.
-            await insertVault(encryptedVaultObject);
-
-            // Create a copy of the encrypted vault, but put in the decrypted data again before inserting into the "vaultStore"
-            const decryptedVaultObject = {...encryptedVaultObject};
-            decryptedVaultObject.key = vaultEncryptionKey;
-            decryptedVaultObject.name = vault.value.name;
-            decryptedVaultObject.description = vault.value.description;
-            decryptedVaultObject.icon = vault.value.icon;
-
-            // Artificial sleep to keep the user waiting...
-            await sleep(1.5);
-
-            vaultStore.add(decryptedVaultObject);
-            vaultStore.setActive(decryptedVaultObject.id);
-
-            isCreatingVault.value = false;
-
-            router.push(PAGES.ROOT);
+        // Handle creating an encrypted token
+        const handleCreateToken = async () => {
+            // Create a copy of the token ref for us to work on
+            const tokenToEncrypt = {...token.value};
         }
 
         return {
@@ -115,10 +153,12 @@ export default defineComponent({
 
             PAGES,
 
-            vault,
-            isCreatingVault,
+            OTPType,
+            OTPAlgorithm,
 
-            handleCreateVault,
+            token,
+
+            handleCreateToken,
         }
     }
 })
