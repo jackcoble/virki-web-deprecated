4b9839cdca3acd88b0ceb4a50616aca98670eeaf
Jack <jack@virki.io>
2022-08-27 20:23:20 +0100

    Display all active sessions in a table

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -19,19 +19,71 @@
 
         <!-- Session management -->
         <div class="flex flex-col flex-1 rounded bg-gray-50 border p-6 border-gray-300 space-y-2">
-            <h1 class="text-lg">Active Sessions</h1>
-
-            <div v-for="session in sessions" :key="session.id" class="flex bg-gray-200 p-2 rounded space-x-8">
-                <!-- Device information -->
-                <div class="flex flex-col">
-                    <p class="font-medium">{{ session.device }}</p>
-                    <p>{{ session.operating_system }}</p>
+            <div class="px-4 sm:px-6 lg:px-8">
+                <div class="sm:flex sm:items-center">
+                    <div class="sm:flex-auto">
+                        <h1 class="text-xl font-semibold text-gray-900">Active Devices</h1>
+                        <p class="mt-2 text-sm text-gray-700">Manage the devices which currently have access to your <span class="text-mountain-meadow">Virki</span> account.</p>
+                    </div>
                 </div>
+                <div class="mt-8 flex flex-col">
+                    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded">
+                                <table class="min-w-full divide-y divide-gray-300">
+                                    <thead class="bg-gray-50">
+                                        <tr>
+                                            <th scope="col"
+                                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                                IP Address</th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Device
+                                            </th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Operating System
+                                            </th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Issued
+                                            </th>
+                                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                Last Active
+                                            </th>
+                                            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                <span class="sr-only">Edit</span>
+                                            </th>
 
-                <!-- Last used -->
-                <div class="flex flex-col">
-                    <p>Last Active: {{ formatDate(session.last_used) }}</p>
-                    <p>Issued: {{ formatDate(session.issued) }}</p>
+                            
+                                        </tr>
+                                    </thead>
+                                    <tbody class="divide-y divide-gray-200 bg-white">
+                                        <tr v-for="session in sessions" :key="session.id">
+                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                {{ session.ip_address }}
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {{ session.device }}
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {{ session.operating_system }}
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {{ formatDate(session.issued) }}
+                                            </td>
+                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {{ formatDate(session.last_used) }}
+                                            </td>
+                                            <td
+                                                class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                <a href="#" class="text-red-500 hover:text-red-600 transition">Revoke</a>
+                                            </td>
+                                        </tr>
+
+                                        <!-- More people... -->
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
