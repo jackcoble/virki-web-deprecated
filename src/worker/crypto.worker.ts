ecc95737d540be6d72f0c9c93707d7a66da3573f
Jack <jack@virki.io>
2022-09-25 18:08:01 +0100

    Update CryptoWorker

--- src/worker/crypto.worker.ts
+++ src/worker/crypto.worker.ts
@@ -1,43 +1,22 @@
 import * as Comlink from "comlink";
-import * as crypto from "@/utils/crypto";
-import type { Keys } from "@/types/user";
-import * as libsodium from "@/utils/crypto/libsodium";
+import { stretchPassword } from "@/common/utils/password";
+import { encryptToB64, encryptUTF8, generateEncryptionKey } from "@/common/utils/sodium";
 
 export class Crypto {
-    async generateKeys (passphrase: string) {
-        return crypto.generateKeys(passphrase);
-    }
-
-    async decryptKeys (passphrase: string, keys: Keys) {
-        return crypto.decryptKeys(passphrase, keys);
-    }
-
-    async decrypt (key: string, cipherString: string) {
-        return crypto.decrypt(key, cipherString);
-    }
-
-    async encryptToB64 (data: string, key?: string) {
-        return libsodium.encryptToB64(data, key);
-    }
-
-    async encryptUTF8 (data: string, key?: string) {
-        return libsodium.encryptUTF8(data, key);
-    }
-
-    async decryptToUTF8(key: string, cipherString: string) {
-        return crypto.decryptToUTF8(key, cipherString)
+    async stretchPassword(password: string) {
+        return stretchPassword(password);
     }
 
     async generateEncryptionKey() {
-        return libsodium.generateEncryptionKey();
+        return generateEncryptionKey();
     }
 
-    async boxSealOpen(input: string, publicKey: string, privateKey: string) {
-        return libsodium.boxSealOpen(input, publicKey, privateKey);
+    async encryptToB64(data: string, key?: string) {
+        return encryptToB64(data, key);
     }
 
-    async fromBase64 (input: string) {
-        return libsodium.fromBase64(input);
+    async encryptUTF8(data: string, key?: string) {
+        return encryptUTF8(data, key);
     }
 }
 
