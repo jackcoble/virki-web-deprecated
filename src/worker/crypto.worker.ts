71a0a61172519c67ad8bf652550b4d6c224c676d
Jack <jack@virki.io>
2022-10-01 19:19:49 +0100

    Update Crypto worker

--- src/worker/crypto.worker.ts
+++ src/worker/crypto.worker.ts
@@ -1,10 +1,10 @@
 import * as Comlink from "comlink";
 import { stretchPassword } from "@/common/utils/password";
-import { encryptToB64, encryptUTF8, generateEncryptionKey, generateKeypair } from "@/common/utils/sodium";
+import { encryptToB64, encryptUTF8, generateEncryptionKey, generateKeypair, decryptFromB64 } from "@/common/utils/sodium";
 
 export class Crypto {
-    async stretchPassword(password: string) {
-        return stretchPassword(password);
+    async stretchPassword(password: string, salt?: string, opsLimit?: number, memLimit?: number) {
+        return stretchPassword(password, salt, opsLimit, memLimit);
     }
 
     async generateEncryptionKey() {
@@ -21,6 +21,10 @@ export class Crypto {
 
     async encryptUTF8(data: string, key?: string) {
         return encryptUTF8(data, key);
+    }
+
+    async decryptFromB64(ciphertext: string, mac: string, nonce: string, key: string) {
+        return decryptFromB64(ciphertext, mac, nonce, key)
     }
 }
 
