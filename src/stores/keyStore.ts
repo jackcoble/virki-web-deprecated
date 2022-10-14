5ec279646c762150b25178a8765fe80ba550257b
Jack <jack@virki.io>
2022-10-14 18:35:05 +0100

    Pinia KeyStore should use "useStorage" for storing the encrypted keys

--- src/stores/keyStore.ts
+++ src/stores/keyStore.ts
@@ -1,23 +1,23 @@
+import { defineStore } from 'pinia';
+import { useStorage } from "@vueuse/core";
+
 import { LocalStorageKeys } from '@/common/enums/localStorage';
 import { SessionStorageKeys } from '@/common/enums/sessionStorage';
-import { LocalStorageService } from '@/common/services/localStorage.service';
 import { SessionStorageService } from '@/common/services/sessionStorage.service';
 import type { Keys } from '@/common/interfaces/keys';
-import { defineStore } from 'pinia'
 import { useUserStore } from './userStore';
 
-const localStorage = new LocalStorageService();
 const sessionStorage = new SessionStorageService();
 
 export const useKeyStore = defineStore({
   id: 'keyStore',
   state: () => ({
     masterEncryptionKey: sessionStorage.get(SessionStorageKeys.MASTER_ENCRYPTION_KEY),
-    encryptedKeys: localStorage.get(LocalStorageKeys.ENCRYPTED_KEYS)
+    encryptedKeys: useStorage(LocalStorageKeys.ENCRYPTED_KEYS, {} as Keys)
   }),
 
   getters: {
-    getSessionToken: (state) => {
+    getSessionToken: () => {
       const userStore = useUserStore();
       return userStore.getSessionToken;
     },
@@ -43,13 +43,12 @@ export const useKeyStore = defineStore({
 
     setEncryptedKeys(keys: Keys) {
         this.encryptedKeys = keys as any;
-        localStorage.add(LocalStorageKeys.ENCRYPTED_KEYS, keys)
     },
 
     // Clears entire store state
     clear() {
       this.masterEncryptionKey = "";
-      this.encryptedKeys = null;
+      this.encryptedKeys = null as any;
     }
   },
 })
