8177f0b02f6ece5838c2f59017260aa8f59de8c4
Jack <jack@virki.io>
2022-10-15 20:40:25 +0100

    Add userStore getter for account plan

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -12,7 +12,8 @@ export const useUserStore = defineStore({
   getters: {
     getUserID: (state) => state.account.id,
     getEmail: (state) => state.account.email,
-    getSessionToken: (state) => state.account.session_token
+    getSessionToken: (state) => state.account.session_token,
+    getPlan: (state) => state.account.plan
   },
 
   actions: {
