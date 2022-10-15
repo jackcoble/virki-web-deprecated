f654b0f58c29161a88b995226469faeb4b5b8b75
Jack <jack@virki.io>
2022-10-15 16:30:37 +0100

    Add sync endpoint to vault service

--- src/service/api/vaultService.ts
+++ src/service/api/vaultService.ts
@@ -5,5 +5,11 @@ import { api } from "./api";
 export default {
     addVault(vault: Vault): Promise<AxiosResponse> {
         return api.post("/v1/vaults", vault);
+    },
+
+    sync(vaults: Vault[]): Promise<AxiosResponse> {
+        return api.post("/v1/sync", {
+            vaults: vaults
+        })
     }
 }
