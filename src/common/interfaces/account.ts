07117ab19445df10e4d0b04b381f7ffa87e47e98
Jack <jack@virki.io>
2022-10-15 20:39:33 +0100

    Add "plan" property to account interface

--- src/common/interfaces/account.ts
+++ src/common/interfaces/account.ts
@@ -1,6 +1,9 @@
+import type { Plan } from "../enums/plans";
+
 export interface Account {
     id: string;
     email: string;
     session_token: string;
+    plan: Plan;
     active_vault_id?: string;
 }
