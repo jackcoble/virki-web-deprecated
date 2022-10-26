6c5c888ef08e09acd2d436815faf3141c1485781
Jack <jack@virki.io>
2022-10-26 21:10:09 +0100

    Show menu toggle icon on mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,35 +1,43 @@
 <template>
     <div
-            class="flex w-full justify-between items-center px-4 md:px-11 py-4 border-b-2 border-b-mountain-meadow bg-gray-100 space-x-3 shadow">
-            <div>
-                <!-- Logo -->
-                <router-link :to="PAGES.ROOT" class="hidden md:block">
-                    <img class="w-24" src="@/assets/images/virki_full_horizontal_transparent_dark.png"
-                        alt="Virki Logo" />
-                </router-link>
-            </div>
+        class="flex w-full justify-between items-center px-4 md:px-11 py-4 border-b-2 border-b-mountain-meadow bg-gray-100 space-x-3 shadow">
+        <div>
+            <!-- Logo -->
+            <router-link :to="PAGES.ROOT" class="hidden md:block">
+                <img class="w-24" src="@/assets/images/virki_full_horizontal_transparent_dark.png" alt="Virki Logo" />
+            </router-link>
 
-            <!-- Avatar -->
-            <div class="flex items-center justify-center space-x-2 rounded px-3 py-1.5 hover:bg-gray-200 transition cursor-pointer" @click="router.push(PAGES.PROFILE)">
-                <UserCircleIcon class="w-7 text-mountain-meadow" />
-                <p class="text-sm">{{ email }}</p>
-            </div>
+            <!-- Menu Icon (only visible on mobile) -->
+            <button class="block md:hidden text-mountain-meadow w-9" @click="closeMenuMobile = !closeMenuMobile">
+                <MenuIcon v-if="!closeMenuMobile" />
+                <XIcon v-else />
+            </button>
         </div>
+
+        <!-- Avatar -->
+        <div class="flex items-center justify-center space-x-2 rounded px-3 py-1.5 hover:bg-gray-200 transition cursor-pointer"
+            @click="router.push(PAGES.PROFILE)">
+            <UserCircleIcon class="w-7 text-mountain-meadow" />
+            <p class="text-sm">{{ email }}</p>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { PAGES } from "@/router/pages";
 import { useUserStore } from '@/stores/userStore';
 
-import { UserCircleIcon } from "@heroicons/vue/solid"
+import { UserCircleIcon, MenuIcon, XIcon } from "@heroicons/vue/solid"
 
 export default defineComponent({
     name: "Navigation",
     components: {
-        UserCircleIcon
+        UserCircleIcon,
+        MenuIcon,
+        XIcon
     },
     setup() {
         const router = useRouter();
@@ -37,8 +45,12 @@ export default defineComponent({
 
         const email = computed(() => userStore.getEmail);
 
+        const closeMenuMobile = ref(false)
+
         return {
             router,
+
+            closeMenuMobile,
 
             email,
             PAGES
