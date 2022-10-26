c509277ced48de61985d349120c7802b404bc605
Jack <jack@virki.io>
2022-10-26 21:13:09 +0100

    Update app store to store state for whether mobile menu should be opened

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -3,16 +3,22 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore({
   id: 'appStore',
   state: () => ({
-    online: navigator.onLine
+    online: navigator.onLine,
+    openMobileMenu: false
   }),
 
   getters: {
-    isOnline: (state) => state.online
+    isOnline: (state) => state.online,
+    shouldOpenMobileMenu: (state) => state.openMobileMenu
   },
 
   actions: {
     setOnline(online: boolean) {
-        this.online = online;
+      this.online = online;
+    },
+
+    setOpenMobileMenu(open: boolean) {
+      this.openMobileMenu = open;
     }
   }
 })
