5042ff52c390a709037a097b8dd4c4e82e6ff797
Jack <jack@virki.io>
2022-10-31 20:25:19 +0000

    Default to "/api" if API_URL variable doesn't exist

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -2,7 +2,7 @@ import { useKeyStore } from "@/stores/keyStore";
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || "/api",
     headers: {
         "Content-Type": "application/json"
     }
