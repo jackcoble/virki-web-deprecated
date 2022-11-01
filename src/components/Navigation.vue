a1899a4a2a037c124d25964045de5230f4becc7f
Jack <jack@virki.io>
2022-11-01 19:26:33 +0000

    Update router links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -30,20 +30,20 @@
                 <MenuItems
                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <MenuItem v-slot="{ active }">
-                    <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
+                    <router-link to="/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
                         <div class="flex space-x-2">
                             <UserCircleIcon class="w-4" />
                             <span>My Profile</span>
                         </div>
-                    </a>
+                    </router-link>
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
-                    <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
+                    <router-link to="/sessions" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
                         <div class="flex space-x-2">
                             <DeviceMobileIcon class="w-4" />
                             <span>Active Sessions</span>
                         </div>
-                    </a>
+                    </router-link>
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
                     <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
