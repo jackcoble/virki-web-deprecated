ba139f5027625ae4411b95ab872b92c624daf2f2
Jack <jack@virki.io>
2022-11-26 18:11:12 +0000

    Enums for DB version and file attachments

---
+++ src/common/enums/database.ts
@@ -0,0 +1,9 @@
+// Database versions for Dexie
+export enum DatabaseVersion {
+    V1 = 1
+}
+
+// File attachment versions (for encryption, formats, etc)
+export enum AttachmentVersion {
+    V1 = 1
+}
