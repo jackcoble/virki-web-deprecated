e6dea4151df82da24cbe2b93e0d4aa24ce208c22
Jack <jack@virki.io>
2022-11-27 16:36:30 +0000

    Handle vault decryption in the layout

--- src/layouts/LayoutVault.vue
+++ src/layouts/LayoutVault.vue
@@ -18,13 +18,17 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 // Components
 import Navigation from '../components/Navigation.vue';
 import Sidebar from '@/components/Sidebar.vue';
 import { useAppStore } from '@/stores/appStore';
+import { useVaultStore } from '@/stores/vaultStore';
+import { CryptoWorker } from '@/common/comlink';
+import { VirkiStorageService } from '@/common/services/storage.service';
+import { useKeyStore } from '@/stores/keyStore';
 
 export default defineComponent({
     name: "LayoutVault",
@@ -34,9 +38,52 @@ export default defineComponent({
     },
     setup() {
         const appStore = useAppStore();
+        const vaultStore = useVaultStore();
+        const keyStore = useKeyStore();
         const route = useRoute();
 
         const openMobileMenu = computed(() => appStore.shouldOpenMobileMenu);
+
+        onMounted(async () => {
+            // Since this is the main "vault" layout, we should do all fetching of encrypted vaults,
+            // and decryption in here...
+            const cryptoWorker = await new CryptoWorker();
+            const storageService = new VirkiStorageService();
+            const existingVaults = await storageService.getVaults();
+
+            // Decrypt the vaults
+            existingVaults.forEach(async encryptedVault => {
+                // Decrypt the vault encryption key
+                const masterEncryptionKey = keyStore.getMasterEncryptionKey;
+                if (!masterEncryptionKey) {
+                    return;
+                }
+
+                // Base64 Encoded Vault Encryption Key
+                const vaultEncryptionKey = await cryptoWorker.decryptFromB64CipherString(encryptedVault.key, masterEncryptionKey)
+
+                // We can proceed to decrypt the UTF8 text, description and icon properties
+                let decryptedName, decryptedDescription, decryptedIcon;
+                decryptedName = await cryptoWorker.decryptFromB64CipherStringToUTF8(encryptedVault.name, vaultEncryptionKey);
+
+                if (encryptedVault.description) {
+                decryptedDescription = await cryptoWorker.decryptFromB64CipherStringToUTF8(encryptedVault.description, vaultEncryptionKey);
+                }
+                if (encryptedVault.icon) {
+                decryptedIcon = await cryptoWorker.decryptFromB64CipherStringToUTF8(encryptedVault.icon, vaultEncryptionKey);
+                }
+
+                // Create a copy of the encrypted vault (to retain all the metadata) and replace the encrypted data with decrypted...
+                const decryptedVault = { ...encryptedVault };
+                decryptedVault.key = vaultEncryptionKey;
+                decryptedVault.name = decryptedName;
+                decryptedVault.description = decryptedDescription;
+                decryptedVault.icon = decryptedIcon;
+
+                // Add the decrypted vault into the Vault Store
+                vaultStore.add(decryptedVault);
+            })
+        })
 
         return {
             openMobileMenu,
