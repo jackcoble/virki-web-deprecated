39639173e011586c1c70eebf31f977c09b17c1ab
Jack <jack@virki.io>
2022-12-21 17:35:05 +0000

    Cosmetic changes to password input component

--- src/components/PasswordInput.vue
+++ src/components/PasswordInput.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="flex justify-between items-center space-x-2">
+    <div class="flex justify-between items-center space-x-1">
         <b-input :type="showPassword ? 'text' : 'password'" :value="modelValue" @input="updateValue"
             :placeholder="placeholder"
             required
         />
         
-        <span @click="showPassword = !showPassword"
-            class="text-mountain-meadow bottom w-5 h-5 cursor-pointer">
-            <EyeOffIcon v-if="showPassword" />
-            <EyeIcon v-if="!showPassword" />
-        </span>
+        <button @click="showPassword = !showPassword"
+            class="text-mountain-meadow bottom cursor-pointer border bg-gray-100 border-gray-300 rounded p-2">
+            <EyeOffIcon class="w-5" v-if="showPassword" />
+            <EyeIcon class="w-5" v-if="!showPassword" />
+        </button>
     </div>
 </template>
 
