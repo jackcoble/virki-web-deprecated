3ca70d4befd64daf22fd3e30a89c3047ee8f0090
Jack <jack@virki.io>
2022-12-21 17:10:26 +0000

    Spice up the email update page slightly

--- src/views/account/Email.vue
+++ src/views/account/Email.vue
@@ -1,4 +1,14 @@
 <template>
-    <h1>Email Address</h1>
-    <p>Update the email address tied to your Virki account.</p>
+    <div class="flex flex-col p-6 pt-12 space-y-2">
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold text-gray-900">Email Address</h1>
+                    <p class="mt-2 text-sm text-gray-700">View or update the email address associated with your
+                        <span class="text-mountain-meadow">Virki</span> account.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
