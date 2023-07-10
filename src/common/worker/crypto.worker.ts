ffa1aa0c194d08c5a871aa00524f981611107186
Jack <jack@virki.io>
2023-07-10 20:48:37 +0100

    CryptoWorker actions should be implemented from an interface

--- src/common/worker/crypto.worker.ts
+++ src/common/worker/crypto.worker.ts
@@ -1,8 +1,26 @@
 import * as Comlink from "comlink";
 import { stretchPassword } from "@/common/utils/password";
 import { encryptToB64, encryptUTF8, generateEncryptionKey, generateKeypair, decryptFromB64, decryptFromB64CipherString, decryptFromB64CipherStringToUTF8, encryptFile, fromBase64, decryptFile } from "@/common/utils/sodium";
+import type { StretchedPassword } from "../interfaces/password";
+import type { StringKeyPair } from "libsodium-wrappers";
+import type { EncryptionResult } from "../interfaces/encryption";
+import type { EncryptedFile } from "../interfaces/file";
 
-export class Crypto {
+export interface ICryptoWorker {
+    stretchPassword(password: string, salt?: string, opsLimit?: number, memLimit?: number): Promise<StretchedPassword>
+    generateEncryptionKey(): Promise<string>
+    generateKeypair(): Promise<StringKeyPair>
+    encryptToB64(data: string, key?: string): Promise<EncryptionResult>
+    encryptUTF8(data: string, key?: string): Promise<EncryptionResult>
+    decryptFromB64(ciphertext: string, mac: string, nonce: string, key: string): Promise<string>
+    decryptFromB64CipherString(cipherString: string, key: string): Promise<string>
+    decryptFromB64CipherStringToUTF8(cipherString: string, key: string): Promise<string>
+    encryptFile(fileType: string, fileContent: Uint8Array, object_key: string, encryptionKey?: string): Promise<EncryptedFile>
+    decryptFile(content: Uint8Array, mime_type: string, encryption_header: string, encryption_key: string): Promise<Blob>
+    fromBase64(input: string): Promise<Uint8Array>
+}
+
+export class Crypto implements ICryptoWorker {
     async stretchPassword(password: string, salt?: string, opsLimit?: number, memLimit?: number) {
         return stretchPassword(password, salt, opsLimit, memLimit);
     }
