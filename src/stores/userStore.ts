fc789dd53a2a5861841a1519fa1b359c5c9155f2
Jack <jack@virki.io>
2023-08-17 18:39:26 +0100

    User store should store access and refresh tokens

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -7,20 +7,31 @@ export const useUserStore = defineStore({
   id: 'userStore',
   state: () => ({
     account: useStorage(LocalStorageKeys.ACCOUNT, {} as Account),
-    avatarUrl: ""
+    avatarUrl: "",
+
+    tokens: useStorage(LocalStorageKeys.TOKENS, {
+      accessToken: null,
+      refreshToken: null
+    } as any)
   }),
 
   getters: {
+    getAccessToken: (state) => state.account.accessToken,
+    getRefreshToken: (state) => state.account.refreshToken,
+
     getUserID: (state) => state.account.id,
     getEmail: (state) => state.account ? state.account.email : "",
     getName: (state) => state.account.name,
-    getSessionToken: (state) => state.account ? state.account.session_token : "",
-    getPlan: (state) => state.account.plan,
-
     getAvatarURL: (state) => state.avatarUrl
   },
 
   actions: {
+    // Persist the access + refresh tokens in LocalStorage
+    setTokens(accessToken: string, refreshToken: string) {
+      this.tokens.accessToken = accessToken;
+      this.tokens.refreshToken = refreshToken;
+    },
+
     setAccount(account: Account) {
       this.account = account;
     },
