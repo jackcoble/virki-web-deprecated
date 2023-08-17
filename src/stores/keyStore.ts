a84404d88611afff1c6cf4f68998953b37a487ee
Jack <jack@virki.io>
2023-08-17 19:15:00 +0100

    Key store should only manage the master encryption key

--- src/stores/keyStore.ts
+++ src/stores/keyStore.ts
@@ -1,53 +1,27 @@
 import { defineStore } from 'pinia';
-import { useStorage, useSessionStorage } from "@vueuse/core";
-
-import { useUserStore } from './userStore';
-import type { Keys } from '@/common/interfaces/keys';
-
-import { LocalStorageKeys, SessionStorageKeys } from '@/common/enums/storage';
+import { useSessionStorage } from "@vueuse/core";
+import { SessionStorageKeys } from '@/common/enums/storage';
 
 export const useKeyStore = defineStore({
   id: 'keyStore',
   state: () => ({
-    master_encryption_key: useSessionStorage(SessionStorageKeys.MASTER_ENCRYPTION_KEY, ""),
-    encrypted_keys: useStorage(LocalStorageKeys.ENCRYPTED_KEYS, {} as Keys)
+    master_encryption_key: useSessionStorage(SessionStorageKeys.MASTER_ENCRYPTION_KEY, "")
   }),
 
   getters: {
-    getSessionToken: () => {
-      const userStore = useUserStore();
-      return userStore.getSessionToken;
-    },
-    getMasterEncryptionKey: (state) => state.master_encryption_key,
-    getEncryptedKeys: (state) => {
-        // So after many hours of tearing my hair out, it turns out WebWorkers hate objects with function signatures being
-        // provided to them! So before returning the encrypted keys, stringify the object into JSON and then parse it again,
-        // just so we have a plain old JSON object.
-        const encryptedKeysStringified = JSON.stringify(state.encrypted_keys);
-        const parsedEncryptedKeys: Keys = JSON.parse(encryptedKeysStringified);
-
-        return parsedEncryptedKeys;
-    }
+    // Returns the raw and plaintext master encryption key inside SessionStorage
+    getMasterEncryptionKey: (state) => state.master_encryption_key
   },
 
   actions: {
-    // Persist the decrypted master key in Session Storage
+    // Persist the decrypted master key in SessionStorage
     setMasterEncryptionKey(key: string) {
       this.master_encryption_key = key;
     },
 
-    setEncryptedKeys(keys: Keys) {
-      this.encrypted_keys = keys;
-    },
-
+    // Removes the decrypted master key in SessionStorage
     clearMasterEncryptionKey() {
       this.master_encryption_key = null as any;
-    },
-
-    // Clears all keys
-    clear() {
-      this.master_encryption_key = null as any;
-      this.encrypted_keys = null as any
     }
   },
 })
