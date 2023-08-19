07e7e53a570daa3ebfc6d880eb1bfb1a1feb37cd
Jack <jack@virki.io>
2023-08-19 15:28:16 +0100

    Prepare a base instance of Axios that has interceptors set up

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,16 @@
+import { useLogout } from "@/composables/useLogout";
+import router from "@/router";
+import { PAGES } from "@/router/pages";
+import type { AxiosError } from "axios";
+import axios from "axios";
 import {
     AccountApi,
     AuthApi,
     Configuration,
     VaultsApi
 } from "virki-axios"
+
+const BASE_PATH = "/api";
 
 export class VirkiAPI {
     private configuration: Configuration;
@@ -17,12 +24,35 @@ export class VirkiAPI {
      */
     constructor() {
         this.configuration = new Configuration({
-            basePath: "/api"
+            basePath: BASE_PATH
         });
 
-        this.authApi = new AuthApi(this.configuration);
-        this.accountApi = new AccountApi(this.configuration);
-        this.vaultsApi = new VaultsApi(this.configuration);
+        // Prepare a base instance of the API configured to handle Refresh tokens
+        const baseApi = axios.create();
+        baseApi.interceptors.response.use(
+            response => {
+                return response;
+            },
+            
+            (error: AxiosError) => {
+                // Something has gone wrong, so lets investigate! First we shall check the status code.
+                // If it is 401, then clear all local data and logout.
+                if (error.response && error.response.status === 401) {
+                    console.log("Unable to refresh tokens, clearing data and logging out...");
+        
+                    useLogout();
+                    router.push(PAGES.LOGIN);
+        
+                    return;
+                }
+        
+                return error;
+            }
+        )
+
+        this.authApi = new AuthApi(this.configuration, BASE_PATH, baseApi);
+        this.accountApi = new AccountApi(this.configuration, BASE_PATH, baseApi);
+        this.vaultsApi = new VaultsApi(this.configuration, BASE_PATH, baseApi);
     }
 
     /**
