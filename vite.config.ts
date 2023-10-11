6f24e2383212cff694e9cec78ff3b871e9ec1f71
Jack Coble <jack@coble.uk>
2023-10-11 18:08:16 +0100

    Use Node global polyfills plugin for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import { VitePWA } from 'vite-plugin-pwa'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,7 +26,17 @@ export default defineConfig({
     target: 'esnext'
   },
   optimizeDeps: {
-    include: ["@/common/worker/crypto.worker"]
+    include: ["@/common/worker/crypto.worker"],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
   },
   server: {
     host: true,
