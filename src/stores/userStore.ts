f094a63cc8394d96806e32c2c54d9ea90a14defe
Jack Coble <jack@coble.uk>
2023-11-05 17:55:16 +0000

    Refactor user store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,47 +1,15 @@
 import { defineStore } from 'pinia'
 import { useStorage } from "@vueuse/core"
 import { LocalStorageKeys } from '@/common/enums/storage';
-import type { AccountInfoResponse } from 'virki-axios';
-import type { Keys } from '@/common/interfaces/keys';
 
 export const useUserStore = defineStore({
   id: 'userStore',
   state: () => ({
-    account: useStorage<AccountInfoResponse>(LocalStorageKeys.ACCOUNT, {}),
-    session: useStorage<string>(LocalStorageKeys.SESSION, null)
+    session: useStorage<string>(LocalStorageKeys.SESSION, "")
   }),
 
   getters: {
-    getSessionToken: (state) => state.session,
-
-    getUserID: (state) => state.account.id,
-    getEmail: (state) => state.account ? state.account.email : "",
-    getName: (state) => state.account.name,
-    
-    getEncryptedKeys: (state) => {
-      const keys: Keys = {
-        kek: {
-          hash: state.account.kek?.hash!,
-          salt: state.account.kek?.salt!,
-          ops_limit: state.account.kek?.opsLimit!,
-          mem_limit: state.account.kek?.memLimit!
-        },
-
-        master_encryption_key: state.account.masterEncryptionKey!,
-  
-        keypair: {
-            public_key: state.account.keypair?.publicKey!,
-            private_key: state.account.keypair?.publicKey!
-        },
-
-        recovery: {
-          master_key_encrypted_with_recovery_key: state.account.recovery?.masterKeyEncryptedWithRecoveryKey!,
-          recovery_key_encrypted_with_master_key: state.account.recovery?.recoveryKeyEncryptedWithMasterKey!
-        }
-      }
-
-      return keys;
-    }
+    getSessionToken: (state) => state.session
   },
 
   actions: {
@@ -50,15 +18,8 @@ export const useUserStore = defineStore({
       this.session = token;
     },
 
-    // Persist the account information response as it
-    // contains basic account information and encrypted keys
-    setAccount(account: AccountInfoResponse) {
-      this.account = account;
-    },
-
     clear() {
-      this.account = null as any;
-      this.session = null as any;
+      this.session = "";
     }
   },
 })
