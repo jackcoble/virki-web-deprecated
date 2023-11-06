dbf00dbdbd93186f5223d0bf9705256b5e5167da
Jack Coble <jack@coble.uk>
2023-11-06 18:03:58 +0000

    Update the user service

--- src/service/api/userService.ts
+++ src/service/api/userService.ts
@@ -1,13 +1,17 @@
 import { api } from "@/service/api/api";
 import type { AxiosResponse } from "axios";
-import type { AccountRegistrationRequestBody, AccountRegistrationResponseBody, GetKeysResponse } from "./types";
+import type { AccountRegistrationRequestBody, AccountRegistrationResponseBody, GetKeysResponse, VaultCreationRequestBody, VaultCreationResponseBody } from "./types";
 
 export default {
     Register(payload: AccountRegistrationRequestBody): Promise<AxiosResponse<AccountRegistrationResponseBody>> {
-        return api.post("/v1/account/register", payload)
+        return api.post("/v1/account/register", payload);
     },
 
     GetKeys(): Promise<AxiosResponse<GetKeysResponse>> {
         return api.get("/v1/account/keys");
+    },
+
+    CreateVault(payload: VaultCreationRequestBody): Promise<AxiosResponse<VaultCreationResponseBody>> {
+        return api.post("/v1/vaults", payload);
     }
 }
