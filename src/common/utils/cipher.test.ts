f6cf490b8d071cad68cf65bbb857919ef390f167
Jack Coble <jack@coble.uk>
2023-11-12 11:44:40 +0000

    Test for cipher parsing to handle unknown encryption type

--- src/common/utils/cipher.test.ts
+++ src/common/utils/cipher.test.ts
@@ -2,12 +2,11 @@ import { describe, expect, test } from "vitest";
 import { parseCipherString } from "./cipher";
 import { EncryptionType } from "../enums/encryptionType";
 
-describe("cipher", () => {
-    test("can parse a valid cipher string", () => {
+describe("cipher string deserialisation", () => {
+    test("can parse a valid xchacha20-poly1305 cipher string", () => {
         const validCipherString = "1.6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=|yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ|4+H4f501w8qpuWrxSYq3vg==";
 
         parseCipherString(validCipherString).then(cipherObject => {
-            // We expect the values to be parsed correctly
             const ciphertext = "6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=";
             const nonce = "yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ";
             const mac = "4+H4f501w8qpuWrxSYq3vg==";
@@ -17,5 +16,10 @@ describe("cipher", () => {
             expect(cipherObject.nonce).toBe(nonce);
             expect(cipherObject.mac).toBe(mac);
         })
+    })
+
+    test("can handle invalid xchacha20-poly1305 cipher string (incorrect encryption type)", async () => {
+        const invalidCipherString = "2.6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=|yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ|4+H4f501w8qpuWrxSYq3vg==";
+        await expect(() => parseCipherString(invalidCipherString)).rejects.toThrowError("Encryption type is invalid!")
     })
 }) 
