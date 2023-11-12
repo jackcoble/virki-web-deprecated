d4aed25c82172934f5c6e4698cd73c4a0a0b049b
Jack Coble <jack@coble.uk>
2023-11-12 12:22:38 +0000

    Simplify the tests

--- src/common/utils/cipher.test.ts
+++ src/common/utils/cipher.test.ts
@@ -2,24 +2,25 @@ import { describe, expect, test } from "vitest";
 import { parseCipherString } from "./cipher";
 import { EncryptionType } from "../enums/encryptionType";
 
-describe("cipher string deserialisation", () => {
-    test("can parse a valid xchacha20-poly1305 cipher string", () => {
-        const validCipherString = "1.6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=|yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ|4+H4f501w8qpuWrxSYq3vg==";
+describe("parseCipherString", () => {
+    test("should parse a valid XCHACHA20_POLY1305 cipher string", async () => {
+        const validCipherString = "1.CipherText|Nonce|MAC";
+        const result = await parseCipherString(validCipherString);
 
-        parseCipherString(validCipherString).then(cipherObject => {
-            const ciphertext = "6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=";
-            const nonce = "yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ";
-            const mac = "4+H4f501w8qpuWrxSYq3vg==";
-
-            expect(cipherObject.type).toBe(EncryptionType.XCHACHA20_POLY1305);
-            expect(cipherObject.ciphertext).toBe(ciphertext);
-            expect(cipherObject.nonce).toBe(nonce);
-            expect(cipherObject.mac).toBe(mac);
+        expect(result).toEqual({
+            type: EncryptionType.XCHACHA20_POLY1305,
+            ciphertext: "CipherText",
+            nonce: "Nonce",
+            mac: "MAC"
         })
     })
 
-    test("can handle invalid xchacha20-poly1305 cipher string (incorrect encryption type)", async () => {
-        const invalidCipherString = "2.6y0awkQtDq6iRDFYIB7AGdrRfeUsqHEXLy2URsBILqs=|yuZOckGPqSaT+xDgMgDqan0s9vMe1TrQ|4+H4f501w8qpuWrxSYq3vg==";
-        await expect(() => parseCipherString(invalidCipherString)).rejects.toThrowError("Encryption type is invalid!")
+    test("should handle invalid encryption type", async () => {
+        const invalidCipherString = "999.CipherText|Nonce|MAC";
+        await expect(parseCipherString(invalidCipherString)).rejects.toEqual("Encryption type is invalid!")
+    })
+
+    test("should handle missing properties depending on the encryption type", () => {
+        // TODO...
     })
 }) 
